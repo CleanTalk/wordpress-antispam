40a23e719ab63bfd518b901a49adbb6c8260efbe
Roman Safronov <r.safronik@gmail.com>
2019-09-12 13:29:38 +0500

    JS maps fix.

--- js/src/cleantalk_external.js
+++ js/src/cleantalk_external.js
@@ -1,26 +1,26 @@
 function ct_protect_external(){
 			
-	for(let i = 0; i < document.forms.length; i++){
+	for(var i = 0; i < document.forms.length; i++){
 		
 		if(typeof(document.forms[i].action) == 'string'){
 			
-			let action = document.forms[i].action;
+			var action = document.forms[i].action;
 
 			if(action.indexOf('http://') !== -1 || action.indexOf('https://') !== -1){
 				
-				let tmp = action.split('//');
+				var tmp = action.split('//');
 				tmp = tmp[1].split('/');
-				let host = tmp[0].toLowerCase();
+				var host = tmp[0].toLowerCase();
 
 				if(host !== location.hostname.toLowerCase()){
 
-                    let ct_action = document.createElement("input");
+                    var ct_action = document.createElement("input");
                     ct_action.name = 'cleantalk_hidden_action';
 					ct_action.value = action;
 					ct_action.type = 'hidden';
 					document.forms[i].appendChild(ct_action);
 
-                    let ct_method = document.createElement("input");
+                    var ct_method = document.createElement("input");
                     ct_method.name = 'cleantalk_hidden_method';
 					ct_method.value = document.forms[i].method;
 					ct_method.type = 'hidden';
