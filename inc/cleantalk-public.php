5ffac6f89935e4344de91a0c7700694168af136a
Viktor <Bazz@bk.ru>
2019-11-19 12:50:00 +0300

    Fix: WooCommenrce registration hook fixed.

--- inc/cleantalk-public.php
+++ inc/cleantalk-public.php
@@ -114,7 +114,7 @@ function apbct_init() {
 
     // WooCommerce registration
     if(class_exists('WooCommerce')){
-	    if( $apbct->settings['wc_register_from_order'] == 1 ){
+	    if( ! is_checkout() && $apbct->settings['wc_checkout_test'] == 0 && $apbct->settings['wc_register_from_order'] == 1 ){
 		    add_filter( 'woocommerce_registration_errors', 'ct_registration_errors', 1, 3 );
 	    }
     }
