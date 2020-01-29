c1344e4f35ba2808cfef75952cefb441472b523f
safronik <safronov@cleantalk.org>
2020-01-30 00:37:18 +0500

    Fix: User exception.

--- inc/cleantalk-public.php
+++ inc/cleantalk-public.php
@@ -3116,7 +3116,7 @@ function ct_contact_form_validate() {
         (isset($_POST['action']) && $_POST['action'] == 'wilcity_login') || // Exception for login form. From Analysis uid=430898
         (isset($_POST['qcfsubmit'])) || //Exception for submit quick forms - duplicates with qcfvalidate
         apbct_is_in_uri('tin-canny-learndash-reporting/src/h5p-xapi/process-xapi-statement.php?v=asd') || //Skip Tin Canny plugin
-        ( isset( $_POST['nhr'], $_POST['ts'] ) && apbct_is_in_uri( '?na=s' ) )  // The Newsletter Plugin double requests fix. Ticket #14772
+        ( isset( $_POST['na'], $_POST['ts'], $_POST['nhr'] ) && !apbct_is_in_uri( '?na=s' ) )  // The Newsletter Plugin double requests fix. Ticket #14772
 		) {
         return null;
     }
