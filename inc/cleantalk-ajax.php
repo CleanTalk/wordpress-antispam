8891d8c18419af1cb722d53d644faa4d141d208c
davydovct <davydovct@cleantalk.org>
2020-02-25 12:06:36 +0500

    Fix skip login form

--- inc/cleantalk-ajax.php
+++ inc/cleantalk-ajax.php
@@ -295,6 +295,7 @@ function ct_ajax_hook($message_obj = false, $additional = false)
 	    'frm_strp_amount', //Admin stripe form
 	    'wouCheckOnlineUsers', //Skip updraft admin checking users
 	    'et_fb_get_shortcode_from_fb_object', //Skip generate shortcode
+	    'pp_lf_process_login', //Skip login form
     );
     
     // Skip test if
