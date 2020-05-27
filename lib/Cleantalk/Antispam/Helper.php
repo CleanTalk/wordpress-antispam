04c0682348376e63c5405228e791dd2a9903e0f5
Viktor <Bazz@bk.ru>
2020-05-27 14:48:43 +0300

    Upd: Helper version updated.

--- lib/Cleantalk/Antispam/Helper.php
+++ lib/Cleantalk/Antispam/Helper.php
@@ -8,7 +8,7 @@ namespace Cleantalk\Antispam;
  *
  * @package       PHP Antispam by CleanTalk
  * @subpackage    Helper
- * @Version       3.2
+ * @Version       3.3
  * @author        Cleantalk team (welcome@cleantalk.org)
  * @copyright (C) 2014 CleanTalk team (http://cleantalk.org)
  * @license       GNU/GPL: http://www.gnu.org/copyleft/gpl.html
