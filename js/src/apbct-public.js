b1ab7b7b404a6b992a0fbca602db7d23e6234ae6
davydovct <davydovct@cleantalk.org>
2020-07-17 15:26:44 +0500

    Fix: exclude caspio form.

--- js/src/apbct-public.js
+++ js/src/apbct-public.js
@@ -92,7 +92,8 @@
 				if (
 					form.classList.contains('slp_search_form') || //StoreLocatorPlus form
 					form.parentElement.classList.contains('mec-booking') ||
-					form.action.toString().indexOf('activehosted.com') // Active Campaign
+					form.action.toString().indexOf('activehosted.com') || // Active Campaign
+					(form.id && form.id == 'caspioform') //Caspio Form
 				)
 					continue;
 
