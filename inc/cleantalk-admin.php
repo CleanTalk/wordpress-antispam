61883e1b739dff2323bba8546a06c0c63ad78501
Glomberg <bazz@bk.ru>
2020-12-14 11:32:52 +0300

    Fix: Users/Comments - additional button removed.

--- inc/cleantalk-admin.php
+++ inc/cleantalk-admin.php
@@ -37,10 +37,6 @@ function apbct_add_buttons_to_comments_and_users( $unused_argument ) {
         <img src="' . $apbct->logo__small__colored . '" alt="Cleantalk Antispam logo"  height="" style="width: 17px; vertical-align: text-bottom;" />
         ' . sprintf(__( 'Find spam %s', 'cleantalk-spam-protect'), $button_description ) . '
     </a>
-    <a href="https://cleantalk.org/my/show_requests?service_id=' . $apbct->data['service_id'] . '&int=week" target="_blank" class="button" style="margin:1px 0 0 0; display: inline-block;">
-        <img src="' . $apbct->logo__small__colored . '" alt="Cleantalk Antispam logo"  height="" style="width: 17px; vertical-align: text-bottom;" />
-        ' . __( 'CleanTalk Anti-Spam Log', 'cleantalk-spam-protect') . '
-    </a>
     ';
 
 }
