da481d3ca489b4ba4bd78693f0da80ddbb41fbcb
Glomberg <bazz@bk.ru>
2021-01-14 11:48:20 +0300

    New: Schema test added.

---
+++ tests/Common/SchemaTest.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once "../lib/autoloader.php";
+
+use Cleantalk\Common\Schema;
+
+class SchemaTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testgetSchema()
+    {
+        $this->expectException(Exception::class);
+        $this->assertIsArray(
+            Schema::getSchema()
+        );
+        $this->assertIsArray(
+            Schema::getSchema('sfw')
+        );
+        $this->assertIsArray(
+            Schema::getSchema(' ')
+        );
+        $this->assertIsArray(
+            Schema::getSchema('')
+        );
+        $this->assertIsArray(
+            Schema::getSchema('wrong')
+        );
+        $this->assertIsArray(
+            Schema::getSchema(false)
+        );
+        $this->assertIsArray(
+            Schema::getSchema(true)
+        );
+    }
+
+}
