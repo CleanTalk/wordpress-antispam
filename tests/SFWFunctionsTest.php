acd5a81d6b6275fb8e47c7690bddb0d649238389
Glomberg <bazz@bk.ru>
2021-02-08 14:41:46 +0300

    Tests: ct_sfw_send_logs test added.

--- tests/SFWFunctionsTest.php
+++ tests/SFWFunctionsTest.php
@@ -3,8 +3,14 @@
 namespace Cleantalk;
 
 class SFWFunctionsTest extends \PHPUnit_Framework_TestCase {
-	public function testApbct_is_remote_call()
+
+	public function testCt_sfw_send_logs()
 	{
-		$this->assertFalse(apbct_is_remote_call());
+		$res = ct_sfw_send_logs( getenv( 'CLEANTALK_TEST_API_KEY') );
+		$this->assertIsArray( $res );
+		$this->assertArrayHasKey( 'rows', $res );
+		$res = ct_sfw_send_logs();
+		$this->assertIsArray( $res );
+		$this->assertArrayHasKey( 'error', $res );
 	}
 }
