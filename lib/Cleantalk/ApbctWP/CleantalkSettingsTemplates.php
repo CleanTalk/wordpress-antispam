990c4079af85e24948f2a327a8a8fe9382c5029a
Glomberg <bazz@bk.ru>
2021-02-15 19:04:06 +0300

    Udp: Settings Templates. Class updated.

--- lib/Cleantalk/ApbctWP/CleantalkSettingsTemplates.php
+++ lib/Cleantalk/ApbctWP/CleantalkSettingsTemplates.php
@@ -8,6 +8,8 @@ class CleantalkSettingsTemplates {
 
 	private $api_key;
 
+	private static $templates = null;
+
 	/**
 	 * CleantalkDefaultSettings constructor.
 	 *
@@ -16,27 +18,39 @@ class CleantalkSettingsTemplates {
 	public function __construct( $api_key )
 	{
 		$this->api_key = $api_key;
-		if( $this->api_key ) {
-			add_filter( 'apbct_settings_action_buttons', array( $this, 'add_action_button' ), 10, 1 );
-		}
+		add_filter( 'apbct_settings_action_buttons', array( $this, 'add_action_button' ), 10, 1 );
+		add_action( 'wp_ajax_get_options_template', array( $this, 'get_options_template_ajax' ) );
+		add_action( 'apbct_settings_template_get', array( $this, 'open_templates_dialog' ) );
 	}
 
-	public function add_action_button( $links ) {
-		$link = '<a href="#" class="ct_support_link" onclick="cleantalkModal.open(\'apbct_settings_templates\')">' . __('Import/Export settings', 'cleantalk-spam-protect') . '</a>';
+	public function add_action_button( $links )
+	{
+		$link = '<a href="#" class="ct_support_link" onclick="cleantalkModal.open()" data-content-action="get_options_template">' . __('Import/Export settings', 'cleantalk-spam-protect') . '</a>';
 		$links[]    = $link;
 		return $links;
 	}
 
-	public static function get_options_template( $api_key ) {
+	public function get_options_template_ajax()
+	{
+		check_ajax_referer('ct_secret_nonce', 'security');
+		echo $this->getHtmlContent();
+		die();
+	}
+
+	public static function get_options_template( $api_key )
+	{
 		// @ToDo here will be API call services_templates_get
 		//\Cleantalk\Common\API::method__services_templates_get( $api_key );
-		return array(
-			1 => array(
-				'template_id' => 1,
-				'name' => 'Test Settings Template',
-				'options_site' => 'json_string',
-			),
-		);
+		if( ! self::$templates ) {
+			self::$templates = array(
+				1 => array(
+					'template_id' => 1,
+					'name' => 'Test Settings Template',
+					'options_site' => 'json_string',
+				),
+			);
+		}
+		return self::$templates;
 	}
 
 	public function getHtmlContent( $import_only = false )
@@ -52,9 +66,9 @@ class CleantalkSettingsTemplates {
 
 	private function getHtmlContentImport( $templates )
 	{
-		$templatesSet = '<p><select>';
+		$templatesSet = '<p><select id="templates_import" >';
 		foreach( $templates as $template ) {
-			$templatesSet .= '<option id="apbcs_settings_template_id_' . $template['template_id'] . '">' . $template['name'] . '</option>';
+			$templatesSet .= '<option data-id="' . $template['template_id'] . '">' . $template['name'] . '</option>';
 		}
 		$templatesSet .= '</select></p>';
 		$button = $this->getImportButton();
@@ -63,12 +77,13 @@ class CleantalkSettingsTemplates {
 
 	public function getHtmlContentExport( $templates )
 	{
-		$templatesSet = '<p><select>';
-		$templatesSet .= '<option checked="true">New template</option>';
+		$templatesSet = '<p><select id="templates_export" >';
+		$templatesSet .= '<option id="new_template" checked="true">New template</option>';
 		foreach( $templates as $template ) {
-			$templatesSet .= '<option id="apbcs_settings_template_id_' . $template['template_id'] . '">' . $template['name'] . '</option>';
+			$templatesSet .= '<option data-id="' . $template['template_id'] . '">' . $template['name'] . '</option>';
 		}
 		$templatesSet .= '</select></p>';
+		$templatesSet .= '<p><input type="text" name="template_name" placeholder="' . esc_html__( 'Enter a template name.', 'cleantalk-spam-protect' ) . '" required /></p>';
 		$button = $this->getExportButton();
 		return $templatesSet . '<br>' . $button . '<br>';
 	}
