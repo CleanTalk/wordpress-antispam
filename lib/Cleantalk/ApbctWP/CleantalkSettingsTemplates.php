ad34fb027486147e10dc9c6ea97bc73e63c328fb
Glomberg <bazz@bk.ru>
2021-02-20 21:42:15 +0300

    Udp: Settings Templates. Main class updated.

--- lib/Cleantalk/ApbctWP/CleantalkSettingsTemplates.php
+++ lib/Cleantalk/ApbctWP/CleantalkSettingsTemplates.php
@@ -20,6 +20,9 @@ class CleantalkSettingsTemplates {
 		$this->api_key = $api_key;
 		add_filter( 'apbct_settings_action_buttons', array( $this, 'add_action_button' ), 10, 1 );
 		add_action( 'wp_ajax_get_options_template', array( $this, 'get_options_template_ajax' ) );
+		add_action( 'wp_ajax_settings_templates_export', array( $this, 'settings_templates_export_ajax' ) );
+		add_action( 'wp_ajax_settings_templates_import', array( $this, 'settings_templates_import_ajax' ) );
+		add_action( 'wp_ajax_settings_templates_reset', array( $this, 'settings_templates_reset_ajax' ) );
 		add_action( 'apbct_settings_template_get', array( $this, 'open_templates_dialog' ) );
 	}
 
@@ -35,6 +38,61 @@ class CleantalkSettingsTemplates {
 		check_ajax_referer('ct_secret_nonce', 'security');
 		echo $this->getHtmlContent();
 		die();
+	}
+
+	public function settings_templates_export_ajax()
+	{
+		check_ajax_referer('ct_secret_nonce', 'security');
+		if( isset( $_POST['data'] ) && is_array( $_POST['data'] ) ) {
+			$template_info = $_POST['data'];
+			if( isset( $template_info['template_id'] ) ) {
+				$template_id = sanitize_text_field( $template_info['template_id'] );
+				$res = \Cleantalk\Common\API::method__services_templates_add( $this->api_key, $template_id );
+				if( empty( $res['error'] ) ) {
+					wp_send_json_success( esc_html__('Success. Reloading...', 'cleantalk-spam-protect' ) );
+				} else {
+					wp_send_json_error( $res['error'] );
+				}
+			}
+			if( isset( $template_info['template_name'] ) ) {
+				$template_name = sanitize_text_field( $template_info['template_name'] );
+				$res = \Cleantalk\Common\API::method__services_templates_add( $this->api_key, $template_name );
+				if( empty( $res['error'] ) ) {
+					wp_send_json_success( esc_html__('Success. Reloading...', 'cleantalk-spam-protect' ) );
+				} else {
+					wp_send_json_error( $res['error'] );
+				}
+			}
+		}
+		wp_send_json_error( 'Export handler error.' );
+	}
+
+	public function settings_templates_import_ajax()
+	{
+		check_ajax_referer('ct_secret_nonce', 'security');
+		if( isset( $_POST['data'] ) && is_array( $_POST['data'] ) ) {
+			$template_info = $_POST['data'];
+			if( isset( $template_info['template_id'], $template_info['template_name'], $template_info['settings'] ) ) {
+				$res = apbct_set_plugin_options( $template_info['template_id'], $template_info['template_name'], $template_info['settings'] );
+				if( empty( $res['error'] ) ) {
+					wp_send_json_success( esc_html__('Success. Reloading...', 'cleantalk-spam-protect' ) );
+				} else {
+					wp_send_json_error( $res['error'] );
+				}
+			}
+		}
+		wp_send_json_error( 'Import handler error.' );
+	}
+
+	public function settings_templates_reset_ajax()
+	{
+		check_ajax_referer('ct_secret_nonce', 'security');
+		$res = apbct_reset_plugin_options();
+		if( empty( $res['error'] ) ) {
+			wp_send_json_success( esc_html__('Success. Reloading...', 'cleantalk-spam-protect' ) );
+		} else {
+			wp_send_json_error( $res['error'] );
+		}
 	}
 
 	public static function get_options_template( $api_key )
@@ -60,15 +118,22 @@ class CleantalkSettingsTemplates {
 		$out = $this->getHtmlContentImport( $templates );
 		if( ! $import_only ) {
 			$out .= $this->getHtmlContentExport( $templates );
+			$out .= $this->getHtmlContentReset();
 		}
 		return $title . '<br>' . $out;
 	}
 
 	private function getHtmlContentImport( $templates )
 	{
-		$templatesSet = '<p><select id="templates_import" >';
+		$templatesSet = '<h3>' . esc_html__( 'Import settings.', 'cleantalk-spam-protect' ) . '</h3>';
+		$templatesSet .= '<p><select id="apbct_settings_templates_import" >';
 		foreach( $templates as $template ) {
-			$templatesSet .= '<option data-id="' . $template['template_id'] . '">' . $template['name'] . '</option>';
+			$templatesSet .= '<option 
+									data-id="' . $template['template_id'] . '"
+									data-name="' . $template['name'] . '"
+									data-settings="' . $template['options_site'] . '">'
+			                 . $template['name']
+			                 . '</option>';
 		}
 		$templatesSet .= '</select></p>';
 		$button = $this->getImportButton();
@@ -77,29 +142,52 @@ class CleantalkSettingsTemplates {
 
 	public function getHtmlContentExport( $templates )
 	{
-		$templatesSet = '<p><select id="templates_export" >';
-		$templatesSet .= '<option id="new_template" checked="true">New template</option>';
+		$templatesSet = '<h3>' . esc_html__( 'Export settings.', 'cleantalk-spam-protect' ) . '</h3>';
+		$templatesSet .= '<p><select id="apbct_settings_templates_export" >';
+		$templatesSet .= '<option data-id="new_template" checked="true">New template</option>';
 		foreach( $templates as $template ) {
 			$templatesSet .= '<option data-id="' . $template['template_id'] . '">' . $template['name'] . '</option>';
 		}
 		$templatesSet .= '</select></p>';
-		$templatesSet .= '<p><input type="text" name="template_name" placeholder="' . esc_html__( 'Enter a template name.', 'cleantalk-spam-protect' ) . '" required /></p>';
+		$templatesSet .= '<p><input type="text" id="apbct_settings_templates_export_name" name="apbct_settings_template_name" placeholder="' . esc_html__( 'Enter a template name.', 'cleantalk-spam-protect' ) . '" required /></p>';
 		$button = $this->getExportButton();
 		return $templatesSet . '<br>' . $button . '<br>';
 	}
 
+	public function getHtmlContentReset()
+	{
+		$content = '<h3>' . esc_html__( 'Reset settings.', 'cleantalk-spam-protect' ) . '</h3>';
+		return '<br><br><hr><br>' . $content . '<br>' .  $this->getResetButton() . '<br>';
+	}
+
 	private function getTitle()
 	{
-		return '<h2>' . esc_html__( 'CkeanTalk settings templates.', 'cleantalk-spam-protect' ) . '</h2>';
+		return '<h2>' . esc_html__( 'CleanTalk settings templates.', 'cleantalk-spam-protect' ) . '</h2>';
 	}
 
 	private function getExportButton()
 	{
-		return '<button id="apbct_settings_templates_export" class="cleantalk_link cleantalk_link-manual">' . esc_html__( 'Export to selected template.', 'cleantalk-spam-protect' ) . '</button>';
+		return '<button id="apbct_settings_templates_export_button" class="cleantalk_link cleantalk_link-manual">'
+		       . esc_html__( 'Export to selected template.', 'cleantalk-spam-protect' )
+		       . '<img style="margin-left: 10px;" class="apbct_preloader_button" src="' . APBCT_URL_PATH . '/inc/images/preloader2.gif" />'
+		       . '<img style="margin-left: 10px;" class="apbct_success --hide" src="' . APBCT_URL_PATH . '/inc/images/yes.png" />'
+		       . '</button>';
 	}
 
 	private function getImportButton(){
-		return '<button id="apbct_settings_templates_import" class="cleantalk_link cleantalk_link-manual">' . esc_html__( 'Import settings from selected template.', 'cleantalk-spam-protect' ) . '</button>';
+		return '<button id="apbct_settings_templates_import_button" class="cleantalk_link cleantalk_link-manual">'
+		       . esc_html__( 'Import settings from selected template.', 'cleantalk-spam-protect' )
+		       . '<img style="margin-left: 10px;" class="apbct_preloader_button" src="' . APBCT_URL_PATH . '/inc/images/preloader2.gif" />'
+		       . '<img style="margin-left: 10px;" class="apbct_success --hide" src="' . APBCT_URL_PATH . '/inc/images/yes.png" />'
+		       . '</button>';
+	}
+
+	private function getResetButton(){
+		return '<button id="apbct_settings_templates_reset_button" class="cleantalk_link cleantalk_link-auto">'
+		       . esc_html__( 'Reset setting to defaults.', 'cleantalk-spam-protect' )
+		       . '<img style="margin-left: 10px;" class="apbct_preloader_button" src="' . APBCT_URL_PATH . '/inc/images/preloader2.gif" />'
+		       . '<img style="margin-left: 10px;" class="apbct_success --hide" src="' . APBCT_URL_PATH . '/inc/images/yes.png" />'
+		       . '</button>';
 	}
 
 }
