339fa21dc284e499ec537df5b5ae7ed1c656cfc9
Glomberg <bazz@bk.ru>
2021-03-16 09:51:26 +0300

    Fix: JS. Show AJAX result fixed.

--- js/src/apbct-public.js
+++ js/src/apbct-public.js
@@ -297,15 +297,9 @@ apbct_showBlockedResponse = function( response ){
 				} )
 			);
 
-			// Create hidden element contains result.
-			var apbct_result = document.createElement( 'div' );
-			apbct_result.setAttribute( 'id', 'apbct-result' );
-			apbct_result.style.display = 'none';
-			apbct_result.innerHTML = response.comment;
-			document.body.append( apbct_result );
-
-			// Show the element
-			cleantalkModal.open('apbct-result');
+			// Show the result by modal
+			cleantalkModal.loaded = response.comment;
+			cleantalkModal.open();
 
 			if(+response.stop_script == 1)
 				window.stop();
