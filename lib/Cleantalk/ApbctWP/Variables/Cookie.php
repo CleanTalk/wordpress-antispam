e9e42171d1dea54964b81f1cfa25d8c1b71e6f10
safronik <Githubishe351@$^>
2021-04-27 13:40:39 +0500

    Mod: Extend \Cleantalk\ApbctWP\Variables to use alternative sessions.

--- lib/Cleantalk/ApbctWP/Variables/Cookie.php
+++ lib/Cleantalk/ApbctWP/Variables/Cookie.php
@@ -3,38 +3,76 @@
 
 namespace Cleantalk\ApbctWP\Variables;
 
+use Cleantalk\ApbctWP\Helper;
 
 class Cookie extends \Cleantalk\Variables\Cookie {
     
-    public static function get( $name ){
+    public static function get( $name, $raw = false ){
     
         global $apbct;
         
         // Return from memory. From $this->variables
-        if(isset(static::$instance->variables[$name]))
-            return static::$instance->variables[$name];
+        if( isset( static::$instance->variables[ $name ] ) ){
+            $value = static::$instance->variables[ $name ];
         
-        // Gettings by alternative way if enabled
-        if( $apbct->settings['data__set_cookies__sessions'] ){
-            $value = apbct_alt_session__get( $name );
-            
-        // The old way
+        // Get from GLOBAL variable
         }else{
-            
-            if( function_exists( 'filter_input' ) )
-                $value = filter_input( INPUT_COOKIE, $name );
+    
+            // Getting by alternative way if enabled
+            if( $apbct->settings['data__set_cookies'] == 2 ){
+                $value = AltSessions::get( $name );
         
-            if( empty( $value ) )
-                $value = isset( $_COOKIE[ $name ] ) ? $_COOKIE[ $name ]	: '';
-            
+                // The old way
+            }else{
+        
+                if( function_exists( 'filter_input' ) ){
+                    $value = filter_input( INPUT_COOKIE, $name );
+                }
+        
+                if( empty( $value ) ){
+                    $value = isset( $_COOKIE[ $name ] ) ? $_COOKIE[ $name ] : '';
+                }
+        
+            }
+    
+            // Remember for further calls
+            static::getInstance()->remember_variable( $name, $value );
         }
-    
-        // Remember for further calls
-        static::getInstance()->remember_variable( $name, $value );
-    
+        
+        // Decoding by default
+        if( ! $raw ){
+            $value = urldecode( $value ); // URL decode
+            $value = Helper::is_json( $value ) ? \GuzzleHttp\json_decode( $value, true ) : $value; // JSON decode
+        }
+        
         return $value;
     }
     
+    /**
+     * Universal method to adding cookies
+     * Using Alternative Sessions or native cookies depends on settings
+     *
+     * @param $name
+     * @param string $value
+     * @param int $expires
+     * @param string $path
+     * @param null $domain
+     * @param bool $secure
+     * @param bool $httponly
+     * @param string $samesite
+     */
+    public static function set ($name, $value = '', $expires = 0, $path = '', $domain = null, $secure = false, $httponly = false, $samesite = 'Lax' ) {
+        
+        global $apbct;
+        
+        if( $apbct->settings['data__set_cookies__sessions'] ){
+            AltSessions::set( $name, $value );
+        }else{
+            self::setNativeCookie( $name, $value, $expires, $path, $domain, $secure, $httponly, $samesite );
+        }
+        
+    }
+
     /**
      * Universal method to adding cookies
      * Wrapper for setcookie() Conisdering PHP version
@@ -52,11 +90,11 @@ class Cookie extends \Cleantalk\Variables\Cookie {
      *
      * @return void
      */
-    public static function set ($name, $value = '', $expires = 0, $path = '', $domain = null, $secure = false, $httponly = false, $samesite = 'Lax' ) {
-        
+    public static function setNativeCookie ($name, $value = '', $expires = 0, $path = '', $domain = null, $secure = false, $httponly = false, $samesite = 'Lax' ) {
+    
         // For PHP 7.3+ and above
         if( version_compare( phpversion(), '7.3.0', '>=' ) ){
-            
+        
             $params = array(
                 'expires'  => $expires,
                 'path'     => $path,
@@ -64,17 +102,19 @@ class Cookie extends \Cleantalk\Variables\Cookie {
                 'secure'   => $secure,
                 'httponly' => $httponly,
             );
-            
+        
             if($samesite)
                 $params['samesite'] = $samesite;
-            
+        
             setcookie( $name, $value, $params );
-            
+        
             // For PHP 5.6 - 7.2
         }else {
             setcookie( $name, $value, $expires, $path, $domain, $secure, $httponly );
         }
         
     }
+    
+    
     
 }
