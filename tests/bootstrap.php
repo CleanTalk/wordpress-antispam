5ece6b89670c18cad4c78e17e5e4500cd3c436f3
Glomberg <bazz@bk.ru>
2021-06-06 13:22:57 +0300

    Test. Adding State class to the test environment.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,10 @@
 require_once dirname( dirname( __FILE__ ) ) . '/lib/autoloader.php';
 require_once dirname( dirname( __FILE__ ) ) . '/lib/cleantalk-php-patch.php';
 
+global $apbct;
+$apbct = new \Cleantalk\ApbctWP\State('cleantalk', array('settings', 'data'));
+\Cleantalk\ApbctWP\State::setDefinitions();
+
 if( ! function_exists( 'add_action' ) ) {
 	function add_action( $arg1, $arg2, $arg3=0, $arg4=0 ) {
 		return null;
