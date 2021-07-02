dc91efa03211d39a3bd64ab7a899c91d458a44a6
Glomberg <bazz@bk.ru>
2021-07-02 17:09:52 +0300

    Tests. Psalm stubs added.

---
+++ tests/psalm-stubs.php
@@ -0,0 +1,16 @@
+<?php
+if( ! defined( 'OBJECT' ) ) {
+	define( 'OBJECT', 'OBJECT' );
+}
+if( ! defined( 'object' ) ) {
+	define( 'object', 'OBJECT' );
+}
+if( ! defined( 'OBJECT_K' ) ) {
+	define( 'OBJECT_K', 'OBJECT_K' );
+}
+if( ! defined( 'ARRAY_A' ) ) {
+	define( 'ARRAY_A', 'ARRAY_A' );
+}
+if( ! defined( 'ARRAY_N' ) ) {
+	define( 'ARRAY_N', 'ARRAY_N' );
+}
