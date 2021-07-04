7d6c6bc3ead885ba119abc58ad4f0d089250bdbd
Glomberg <bazz@bk.ru>
2021-07-04 18:38:12 +0300

    Tests. Psalm fixed.

--- tests/wp-test-setup.sh
+++ tests/wp-test-setup.sh
@@ -6,10 +6,10 @@
 # License: GPL3
 
 export WP_CORE_DIR=/tmp/wordpress
-export WP_TESTS_DIR=/tmp/wordpress-tests
+#export WP_TESTS_DIR=/tmp/wordpress-tests
 
 # Init database
-mysql -e 'CREATE DATABASE wordpress_test;' -uroot
+#mysql -e 'CREATE DATABASE wordpress_test;' -uroot
 
 # Grab specified version of WordPress from github
 wget -nv -O /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz
@@ -17,9 +17,9 @@ mkdir -p $WP_CORE_DIR
 tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
 
 # Grab testing framework and config file
-svn co --quiet --ignore-externals http://unit-tests.svn.wordpress.org/trunk/ $WP_TESTS_DIR
+#svn co --quiet --ignore-externals http://unit-tests.svn.wordpress.org/trunk/ $WP_TESTS_DIR
 
-wget -nv -O $WP_TESTS_DIR/wp-tests-config.php https://raw.github.com/tierra/wordpress-plugin-tests/setup/wp-tests-config.php
+#wget -nv -O $WP_TESTS_DIR/wp-tests-config.php https://raw.github.com/tierra/wordpress-plugin-tests/setup/wp-tests-config.php
 
 # Put various components in proper folders
 plugin_slug=$(basename $(pwd))
