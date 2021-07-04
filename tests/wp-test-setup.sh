8b525cfd2f6e44c0ca086b80638ac6ffc2be8f9e
Glomberg <bazz@bk.ru>
2021-07-04 18:29:31 +0300

    Tests. TravisCI config fixed.

--- tests/wp-test-setup.sh
+++ tests/wp-test-setup.sh
@@ -12,7 +12,7 @@ export WP_TESTS_DIR=/tmp/wordpress-tests
 mysql -e 'CREATE DATABASE wordpress_test;' -uroot
 
 # Grab specified version of WordPress from github
-wget -nv -O /tmp/wordpress.tar.gz https://github.com/WordPress/WordPress/tarball/$WP_VERSION
+wget -nv -O /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz
 mkdir -p $WP_CORE_DIR
 tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
 
