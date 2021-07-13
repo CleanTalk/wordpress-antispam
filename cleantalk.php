1df40f28c201182775826d7c6b74d60ccf36ce11
Glomberg <bazz@bk.ru>
2021-07-13 10:32:05 +0300

    Code. Activation refactored.

--- cleantalk.php
+++ cleantalk.php
@@ -10,12 +10,14 @@
   Domain Path: /i18n
 */
 
+use Cleantalk\ApbctWP\Activator;
 use Cleantalk\ApbctWP\AdminNotices;
 use Cleantalk\ApbctWP\CleantalkUpgrader;
 use Cleantalk\ApbctWP\CleantalkUpgraderSkin;
 use Cleantalk\ApbctWP\CleantalkUpgraderSkin_Deprecated;
 use Cleantalk\ApbctWP\Cron;
 use Cleantalk\ApbctWP\DB;
+use Cleantalk\ApbctWP\Deactivator;
 use Cleantalk\ApbctWP\Firewall\AntiCrawler;
 use Cleantalk\ApbctWP\Firewall\SFW;
 use Cleantalk\ApbctWP\Helper;
@@ -291,16 +293,16 @@ if( ! is_admin() && ! apbct_is_ajax() && ! apbct_is_customize_preview() ){
 // http://codex.wordpress.org/Function_Reference/register_activation_hook
 register_activation_hook( __FILE__, 'apbct_activation' );
 function apbct_activation( $network_wide ) {
-	\Cleantalk\ApbctWP\Activator::activation( $network_wide );
+	Activator::activation( $network_wide );
 }
 register_deactivation_hook( __FILE__, 'apbct_deactivation' );
 function apbct_deactivation( $network_wide ) {
-	\Cleantalk\ApbctWP\Deactivator::deactivation( $network_wide );
+	Deactivator::deactivation( $network_wide );
 }
 // Hook for newly added blog
 add_action('wpmu_new_blog', 'apbct_activation__new_blog', 10, 6);
 function apbct_activation__new_blog( $blog_id, $_user_id, $_domain, $_path, $_site_id, $_meta ) {
-	\Cleantalk\ApbctWP\Activator::activation( false, $blog_id );
+	Activator::activation( false, $blog_id );
 }
 
 // Async loading for JavaScript
@@ -561,117 +563,16 @@ function apbct_sfw__check()
 }
 
 /**
- * On activation, set a time, frequency and name of an action hook to be scheduled.
- * @throws Exception
+ * Creating specific tables
+ *
+ * @param $sqls
+ * @param string $db_prefix
+ *
+ * @return void
+ * @depreacted Use Activator::create_tables() instead
  */
-function _apbct_activation( $network = false ) {
-	
-	global $wpdb, $apbct;
-	
-	$apbct->stats['plugin']['activation_previous__timestamp'] = $apbct->stats['plugin']['activation__timestamp'];
-	$apbct->stats['plugin']['activation__timestamp'] = time();
-	$apbct->stats['plugin']['activation__times'] += 1;
-    $apbct->save('stats');
-	
-	$sqls = Schema::getSchema();
-	$ct_cron = new Cron();
-
-	if($network && !defined('CLEANTALK_ACCESS_KEY')){
-		$initial_blog  = get_current_blog_id();
-		$blogs = array_keys($wpdb->get_results('SELECT blog_id FROM '. $wpdb->blogs, OBJECT_K));
-		foreach ($blogs as $blog) {
-			switch_to_blog($blog);
-			apbct_activation__create_tables($sqls);
-			// Cron tasks
-
-			$ct_cron->addTask('check_account_status',  'ct_account_status_check',        3600, time() + 1800); // Checks account status
-			$ct_cron->addTask('delete_spam_comments',  'ct_delete_spam_comments',        3600, time() + 3500); // Formerly ct_hourly_event_hook()
-			$ct_cron->addTask('send_feedback',         'ct_send_feedback',               3600, time() + 3500); // Formerly ct_hourly_event_hook()
-			$ct_cron->addTask('sfw_update',            'apbct_sfw_update__init',         86400 );  // SFW update
-			$ct_cron->addTask('send_sfw_logs',         'ct_sfw_send_logs',               3600, time() + 1800); // SFW send logs
-			$ct_cron->addTask('get_brief_data',        'cleantalk_get_brief_data',       86400, time() + 3500); // Get data for dashboard widget
-			$ct_cron->addTask('send_connection_report','ct_mail_send_connection_report', 86400, time() + 3500); // Send connection report to welcome@cleantalk.org
-			$ct_cron->addTask('antiflood__clear_table',  'apbct_antiflood__clear_table',        86400,    time() + 300); // Clear Anti-Flood table
-		}
-		switch_to_blog($initial_blog);
-	}else{
-		
-		// Cron tasks
-		$ct_cron->addTask('check_account_status',  'ct_account_status_check',        3600, time() + 1800); // Checks account status
-		$ct_cron->addTask('delete_spam_comments',  'ct_delete_spam_comments',        3600, time() + 3500); // Formerly ct_hourly_event_hook()
-		$ct_cron->addTask('send_feedback',         'ct_send_feedback',               3600, time() + 3500); // Formerly ct_hourly_event_hook()
-		$ct_cron->addTask('sfw_update',            'apbct_sfw_update__init',         86400 );  // SFW update
-		$ct_cron->addTask('send_sfw_logs',         'ct_sfw_send_logs',               3600, time() + 1800); // SFW send logs
-		$ct_cron->addTask('get_brief_data',        'cleantalk_get_brief_data',       86400, time() + 3500); // Get data for dashboard widget
-		$ct_cron->addTask('send_connection_report','ct_mail_send_connection_report', 86400, time() + 3500); // Send connection report to welcome@cleantalk.org
-		$ct_cron->addTask('antiflood__clear_table',  'apbct_antiflood__clear_table',        86400,    time() + 300); // Clear Anti-Flood table
-  
-		apbct_activation__create_tables($sqls);
-		ct_account_status_check(null, false);
-	}
-	
-	// Additional options
-	add_option( 'ct_plugin_do_activation_redirect', true );
-    apbct_add_admin_ip_to_swf_whitelist( null, null );
-
-}
-
 function apbct_activation__create_tables( $sqls, $db_prefix = '' ) {
-
-	if( ! is_array( $sqls ) && empty( $sqls ) ) {
-		return;
-	}
-
-    global $wpdb;
-    
-    $db_prefix = $db_prefix ? $db_prefix : $wpdb->prefix;
-    
-	$wpdb->show_errors = false;
-	foreach($sqls as $sql){
-		$sql = sprintf($sql, $db_prefix); // Adding current blog prefix
-		$result = $wpdb->query($sql);
-		if($result === false)
-			$errors[] = "Failed.\nQuery: {$wpdb->last_query}\nError: {$wpdb->last_error}";
-	}
-	$wpdb->show_errors = true;
-	
-	// Logging errors
-	if(!empty($errors))
-		apbct_log($errors);
-}
-
-/**
- * On activation, set a time, frequency and name of an action hook to be scheduled for sub-sites.
- * @throws Exception
- */
-function _apbct_activation__new_blog($blog_id, $user_id, $domain, $path, $site_id, $meta) {
-    if (apbct_is_plugin_active_for_network('cleantalk-spam-protect/cleantalk.php')){
-
-		$settings = get_option('cleantalk_settings');
-
-        switch_to_blog($blog_id);
-
-        $sqls = Schema::getSchema();
-
-	    $ct_cron = new Cron();
-
-		// Cron tasks
-	    $ct_cron->addTask('check_account_status',  'ct_account_status_check',        3600, time() + 1800); // Checks account status
-	    $ct_cron->addTask('delete_spam_comments',  'ct_delete_spam_comments',        3600, time() + 3500); // Formerly ct_hourly_event_hook()
-	    $ct_cron->addTask('send_feedback',         'ct_send_feedback',               3600, time() + 3500); // Formerly ct_hourly_event_hook()
-	    $ct_cron->addTask('send_sfw_logs',         'ct_sfw_send_logs',               3600, time() + 1800); // SFW send logs
-	    $ct_cron->addTask('get_brief_data',        'cleantalk_get_brief_data',       86400, time() + 3500); // Get data for dashboard widget
-	    $ct_cron->addTask('send_connection_report','ct_mail_send_connection_report', 86400, time() + 3500); // Send connection report to welcome@cleantalk.org
-	    $ct_cron->addTask('antiflood__clear_table',  'apbct_antiflood__clear_table',        86400,    time() + 300); // Clear Anti-Flood table
-		apbct_activation__create_tables($sqls);
-        apbct_sfw_update__init( 3 ); // Updating SFW
-		ct_account_status_check(null, false);
-
-		if (isset($settings['multisite__use_settings_template_apply_for_new']) && $settings['multisite__use_settings_template_apply_for_new'] == 1) {
-			update_option('cleantalk_settings', $settings);
-		}
-        restore_current_blog();
-    }
+	Activator::create_tables( $sqls, $db_prefix );
 }
 
 /**
