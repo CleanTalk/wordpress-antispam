4eeb093dc3270d86d98984f7fa21937ea2c5ec5b
Glomberg <bazz@bk.ru>
2021-07-18 11:02:43 +0300

    Fix. Code. cleantalk-php-patch.php normalized.

--- lib/cleantalk-php-patch.php
+++ lib/cleantalk-php-patch.php
@@ -12,7 +12,7 @@ if( !function_exists('apache_request_headers') ){
 			if(preg_match('/\AHTTP_/', $key)){
 				$server_key = preg_replace('/\AHTTP_/', '', $key);
 				$key_parts = explode('_', $server_key);
-				if(count($key_parts) > 0 and strlen($server_key) > 2){
+				if(strlen($server_key) > 2){
 					foreach($key_parts as $part_index => $part){
 						$key_parts[$part_index] = function_exists('mb_strtolower') ? mb_strtolower($part) : strtolower($part);
 						$key_parts[$part_index][0] = strtoupper($key_parts[$part_index][0]);					
@@ -31,7 +31,7 @@ if( !function_exists('apache_request_headers') ){
  * For old PHP versions
  */
 if( !function_exists('locale_get_display_region') ){
-	function locale_get_display_region($locale, $in_locale = 'EN'){
+	function locale_get_display_region($locale, $_in_locale = 'EN'){
 		
 		return 'Unkonwn' . ($locale ? ': ' . $locale : '');
 	}
