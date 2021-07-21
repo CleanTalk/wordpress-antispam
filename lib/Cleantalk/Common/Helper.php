cd1b15476ccc2670997237c7d019d079365033d0
Glomberg <bazz@bk.ru>
2021-07-21 22:12:51 +0300

    New. Helper. Curl multi wrapper added.

--- lib/Cleantalk/Common/Helper.php
+++ lib/Cleantalk/Common/Helper.php
@@ -668,6 +668,63 @@ class Helper
 		
 		return $out;
 	}
+
+	/**
+	 * @param array $urls
+	 *
+	 * @return array
+	 */
+	public static function http__multi_request( $urls )
+	{
+		if( ! is_array( $urls ) || empty( $urls ) ) {
+			return array( 'error' => 'CURL_MULTI: Parameter is not an array.' );
+		}
+
+		foreach( $urls as $url ) {
+			if( ! is_string( $url ) ) {
+				return array( 'error' => 'CURL_MULTI: Parameter elements must be strings.' );
+			}
+		}
+
+		$urls_count = count( $urls );
+		$curl_arr = array();
+		$master = curl_multi_init();
+
+		for($i = 0; $i < $urls_count; $i++)
+		{
+			$url =$urls[$i];
+			$curl_arr[$i] = curl_init($url);
+			$opts = array(
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_CONNECTTIMEOUT_MS => 10000,
+				CURLOPT_FORBID_REUSE => true,
+				CURLOPT_USERAGENT => self::AGENT . '; ' . ( isset( $_SERVER['REMOTE_ADDR'] ) ? $_SERVER['REMOTE_ADDR'] : 'UNKNOWN_HOST' ),
+				CURLOPT_HTTPHEADER => array('Expect:'), // Fix for large data and old servers http://php.net/manual/ru/function.curl-setopt.php#82418
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_MAXREDIRS => 5,
+			);
+			curl_setopt_array($curl_arr[$i], $opts);
+			curl_multi_add_handle($master, $curl_arr[$i]);
+		}
+
+		do {
+			curl_multi_exec($master,$running);
+			// @ToDo place here sleep(500) to avoid possible CPU overusing
+		} while($running > 0);
+
+		$results = array();
+
+		for($i = 0; $i < $urls_count; $i++)
+		{
+			$info = curl_getinfo($curl_arr[$i], CURLINFO_HTTP_CODE);
+			if( 200 == $info ) {
+				$results[] = curl_multi_getcontent( $curl_arr[$i] );;
+			} else {
+				$results[] = 'error';
+			}
+		}
+		return $results;
+	}
 	
 	/**
 	 * Merging arrays without reseting numeric keys
