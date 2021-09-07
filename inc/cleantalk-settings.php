dc316b12688420302ad7379ddc4f5e5d780f61e6
Glomberg <bazz@bk.ru>
2021-09-07 19:19:57 +0300

    Fix. WPMS. Key field displaying fixed.

--- inc/cleantalk-settings.php
+++ inc/cleantalk-settings.php
@@ -66,7 +66,7 @@ function apbct_settings__set_fileds( ){
 					'callback'    => 'apbct_settings__field__statistics',
 				),
 				'api_key' => array(
-					'display'        => !$apbct->white_label || is_main_site(),
+					'display'        => $apbct->multisite__work_mode != 2 || is_main_site(),
 					'callback'       => 'apbct_settings__field__apikey',
 				),
 			),
