14ac58a969b3d773462f1720eebac44cc5485696
Glomberg <bazz@bk.ru>
2021-09-10 16:55:22 +0300

    Code: PSR12. lib/Cleantalk/ApbctWP/RestController.php.

--- lib/Cleantalk/ApbctWP/RestController.php
+++ lib/Cleantalk/ApbctWP/RestController.php
@@ -2,27 +2,27 @@
 
 namespace Cleantalk\ApbctWP;
 
-class RestController extends \WP_REST_Controller {
+class RestController extends \WP_REST_Controller
+{
+    public function __construct()
+    {
+        $this->namespace = 'cleantalk-antispam/v1';
+    }
 
-	public function __construct()
-	{
-		$this->namespace = 'cleantalk-antispam/v1';
-	}
-
-	public function register_routes()
-	{
-		register_rest_route( $this->namespace, "/js_keys__get", array(
-			array(
-				'methods'             => 'POST',
-				'callback'            => 'apbct_js_keys__get__ajax',
-				'permission_callback' => '__return_true',
-			)
-		) );
-        
-        register_rest_route( $this->namespace, "/alt_sessions", array(
+    public function register_routes() // phpcs:ignore PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+    {
+        register_rest_route($this->namespace, "/js_keys__get", array(
             array(
                 'methods'             => 'POST',
-                'callback'            => array( \Cleantalk\ApbctWP\Variables\AltSessions::class, 'setFromRemote'),
+                'callback'            => 'apbct_js_keys__get__ajax',
+                'permission_callback' => '__return_true',
+            )
+        ));
+
+        register_rest_route($this->namespace, "/alt_sessions", array(
+            array(
+                'methods'             => 'POST',
+                'callback'            => array(\Cleantalk\ApbctWP\Variables\AltSessions::class, 'setFromRemote'),
                 'args'                => array(
                     'cookies' => array(
                         'type'     => 'array',
@@ -33,7 +33,7 @@ class RestController extends \WP_REST_Controller {
             ),
             array(
                 'methods'             => 'GET',
-                'callback'            => array( \Cleantalk\ApbctWP\Variables\AltSessions::class, 'getFromRemote'),
+                'callback'            => array(\Cleantalk\ApbctWP\Variables\AltSessions::class, 'getFromRemote'),
                 'args'                => array(
                     'name' => array(
                         'type'     => 'string',
@@ -42,7 +42,6 @@ class RestController extends \WP_REST_Controller {
                 ),
                 'permission_callback' => '__return_true',
             )
-        ) );
-	}
-
+        ));
+    }
 }
