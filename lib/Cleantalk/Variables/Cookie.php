e8351a63cc81e411bdee4bef172f5b6c1a42ad07
Glomberg <bazz@bk.ru>
2021-09-10 13:24:44 +0300

    Code: PSR12. lib/Cleantalk/Variables/Cookie.php.

--- lib/Cleantalk/Variables/Cookie.php
+++ lib/Cleantalk/Variables/Cookie.php
@@ -10,98 +10,103 @@ namespace Cleantalk\Variables;
  *
  * @package Cleantalk\Variables
  */
-class Cookie extends ServerVariables{
-	
-	public static $instance;
-	
-	/**
-	 * Constructor
-	 * @return $this
-	 */
-	public static function getInstance(){
-		if (!isset(static::$instance)) {
-			static::$instance = new static;
-			static::$instance->init();
-		}
-		return static::$instance;
-	}
-	
-	/**
-	 * Gets given $_COOKIE variable and save it to memory
-	 * @param $name
-	 *
-	 * @return mixed|string
-	 */
-	protected function getVariable( $name ){
-		
-		// Return from memory. From $this->variables
-		if( ! isset( static::$instance->variables[ $name ] ) ) {
-			if ( function_exists( 'filter_input' ) ) {
-				$value = filter_input( INPUT_COOKIE, $name );
-			}
+class Cookie extends ServerVariables
+{
+    public static $instance;
 
-			if ( empty( $value ) ) {
-				$value = isset( $_COOKIE[ $name ] ) ? $_COOKIE[ $name ] : '';
-			}
+    /**
+     * Constructor
+     * @return $this
+     */
+    public static function getInstance()
+    {
+        if (! isset(static::$instance)) {
+            static::$instance = new static();
+            static::$instance->init();
+        }
 
-			// Remember for further calls
-			static::getInstance()->rememberVariable( $name, $value );
+        return static::$instance;
+    }
 
-			return $value;
-		}
+    /**
+     * Gets given $_COOKIE variable and save it to memory
+     *
+     * @param $name
+     *
+     * @return mixed|string
+     */
+    protected function getVariable($name)
+    {
+        // Return from memory. From $this->variables
+        if (! isset(static::$instance->variables[$name])) {
+            if (function_exists('filter_input')) {
+                $value = filter_input(INPUT_COOKIE, $name);
+            }
 
-		return static::$instance->variables[ $name ];
+            if (empty($value)) {
+                $value = isset($_COOKIE[$name]) ? $_COOKIE[$name] : '';
+            }
 
-	}
+            // Remember for further calls
+            static::getInstance()->rememberVariable($name, $value);
 
-	/**
-	 * Universal method to adding cookies
-	 * Wrapper for setcookie() Conisdering PHP version
-	 *
-	 * @see https://www.php.net/manual/ru/function.setcookie.php
-	 *
-	 * @param string $name     Cookie name
-	 * @param string $value    Cookie value
-	 * @param int    $expires  Expiration timestamp. 0 - expiration with session
-	 * @param string $path
-	 * @param string $domain
-	 * @param bool   $secure
-	 * @param bool   $httponly
-	 * @param string $samesite
-	 *
-	 * @return void
-	 * @psalm-suppress PossiblyUnusedMethod
-	 */
-	public static function set( $name, $value = '', $expires = 0, $path = '', $domain = '', $secure = null, $httponly = false, $samesite = 'Lax' ) {
-        
-        $secure = ! is_null( $secure ) ? $secure : Server::get('HTTPS') !== 'off' || Server::get('SERVER_PORT') == 443;
+            return $value;
+        }
 
-		// For PHP 7.3+ and above
-		if( version_compare( phpversion(), '7.3.0', '>=' ) ){
+        return static::$instance->variables[$name];
+    }
 
-			$params = array(
-				'expires'  => $expires,
-				'path'     => $path,
-				'domain'   => $domain,
-				'secure'   => $secure,
-				'httponly' => $httponly,
-			);
+    /**
+     * Universal method to adding cookies
+     * Wrapper for setcookie() Conisdering PHP version
+     *
+     * @see https://www.php.net/manual/ru/function.setcookie.php
+     *
+     * @param string $name Cookie name
+     * @param string $value Cookie value
+     * @param int $expires Expiration timestamp. 0 - expiration with session
+     * @param string $path
+     * @param string $domain
+     * @param bool $secure
+     * @param bool $httponly
+     * @param string $samesite
+     *
+     * @return void
+     * @psalm-suppress PossiblyUnusedMethod
+     */
+    public static function set(
+        $name,
+        $value = '',
+        $expires = 0,
+        $path = '',
+        $domain = '',
+        $secure = null,
+        $httponly = false,
+        $samesite = 'Lax'
+    ) {
+        $secure = ! is_null($secure) ? $secure : Server::get('HTTPS') !== 'off' || Server::get('SERVER_PORT') == 443;
 
-			if($samesite) {
-				$params['samesite'] = $samesite;
-			}
+        // For PHP 7.3+ and above
+        if (version_compare(phpversion(), '7.3.0', '>=')) {
+            $params = array(
+                'expires' => $expires,
+                'path' => $path,
+                'domain' => $domain,
+                'secure' => $secure,
+                'httponly' => $httponly,
+            );
 
-			/**
-			 * @psalm-suppress InvalidArgument
-			 */
-			setcookie( $name, $value, $params );
+            if ($samesite) {
+                $params['samesite'] = $samesite;
+            }
 
-			// For PHP 5.6 - 7.2
-		}else {
-			setcookie( $name, $value, $expires, $path, $domain, $secure, $httponly );
-		}
-
-	}
-
-
+            /**
+             * @psalm-suppress InvalidArgument
+             */
+            setcookie($name, $value, $params);
+            // For PHP 5.6 - 7.2
+        } else {
+            setcookie($name, $value, $expires, $path, $domain, $secure, $httponly);
+        }
+    }
 }
