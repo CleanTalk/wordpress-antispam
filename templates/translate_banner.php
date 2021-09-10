7ecf334ac60930c7f5fa5e6d674f482b4d15be15
Glomberg <bazz@bk.ru>
2021-09-10 11:38:30 +0300

    Code: PSR12. templates/translate_banner.php.

--- templates/translate_banner.php
+++ templates/translate_banner.php
@@ -1,14 +1,21 @@
 <?php
+
 $ct_translate_banner_template = '
 <div class="ct_settings_banner" id="ct_translate_plugin">
     <div class="ct_rate_block">'
-        .'<p>'
-			.__('Help others use the plugin in your language.', 'cleantalk-spam-protect')
-			.__('We ask you to help with the translation of the plugin in your language. Please take a few minutes to make the plugin more comfortable.', 'cleantalk-spam-protect')
-		.'</p>
+                                . '<p>'
+                                . __('Help others use the plugin in your language.', 'cleantalk-spam-protect')
+                                . __(
+                                    'We ask you to help with the translation of the plugin in your language. 
+                                    Please take a few minutes to make the plugin more comfortable.',
+                                    'cleantalk-spam-protect'
+                                )
+                                . '</p>
         <div>
-            <a href="https://translate.wordpress.org/locale/%s/default/wp-plugins/cleantalk-spam-protect" target="_blank">'.__('TRANSLATE', 'cleantalk-spam-protect').'</a>
+            <a 
+            href="https://translate.wordpress.org/locale/%s/default/wp-plugins/cleantalk-spam-protect" 
+            target="_blank">' . __('TRANSLATE', 'cleantalk-spam-protect') . '
+            </a>
         </div>
     </div>
 </div>';
-?>
