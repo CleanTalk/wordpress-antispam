b85439d4d3f77112f970bf50cd1ed79d072ff4de
Glomberg <bazz@bk.ru>
2021-09-12 13:52:08 +0300

    Code: PSR12. lib/Cleantalk/ApbctWP/Cron.php.

--- lib/Cleantalk/ApbctWP/Cron.php
+++ lib/Cleantalk/ApbctWP/Cron.php
@@ -2,52 +2,55 @@
 
 namespace Cleantalk\ApbctWP;
 
-class Cron extends \Cleantalk\Common\Cron {
+class Cron extends \Cleantalk\Common\Cron
+{
+    /**
+     * Get timestamp last Cron started.
+     *
+     * @return int timestamp
+     */
+    public function getCronLastStart()
+    {
+        global $apbct;
 
-	/**
-	 * Get timestamp last Cron started.
-	 *
-	 * @return int timestamp
-	 */
-	public function getCronLastStart()
-	{
-		global $apbct;
-		return $apbct->stats['cron']['last_start'];
-	}
+        return $apbct->stats['cron']['last_start'];
+    }
 
-	/**
-	 * Save timestamp of running Cron.
-	 *
-	 * @return bool
-	 */
-	public function setCronLastStart()
-	{
-		global $apbct;
-		$apbct->stats['cron']['last_start'] = time();
-		$apbct->save('stats');
-		return true;
-	}
+    /**
+     * Save timestamp of running Cron.
+     *
+     * @return bool
+     */
+    public function setCronLastStart()
+    {
+        global $apbct;
+        $apbct->stats['cron']['last_start'] = time();
+        $apbct->save('stats');
 
-	/**
-	 * Save option with tasks
-	 *
-	 * @param array $tasks
-	 *
-	 * @return bool
-	 */
-	public function saveTasks( $tasks )
-	{
-		return update_option( $this->cron_option_name, $tasks );
-	}
+        return true;
+    }
 
-	/**
-	 * Getting all tasks
-	 *
-	 * @return array
-	 */
-	public function getTasks()
-	{
-		$tasks = get_option( $this->cron_option_name );
-		return empty( $tasks ) ? array() : $tasks;
-	}
+    /**
+     * Save option with tasks
+     *
+     * @param array $tasks
+     *
+     * @return bool
+     */
+    public function saveTasks($tasks)
+    {
+        return update_option($this->cron_option_name, $tasks);
+    }
+
+    /**
+     * Getting all tasks
+     *
+     * @return array
+     */
+    public function getTasks()
+    {
+        $tasks = get_option($this->cron_option_name);
+
+        return empty($tasks) ? array() : $tasks;
+    }
 }
