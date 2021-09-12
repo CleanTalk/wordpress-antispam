eff146374a808748d9361df0bdc54358c576ca6f
Glomberg <bazz@bk.ru>
2021-09-12 20:11:05 +0300

    Code: PSR12. lib/autoloader.php.

--- lib/autoloader.php
+++ lib/autoloader.php
@@ -7,17 +7,15 @@
  *
  * @return void
  */
-spl_autoload_register( function( $class ){
-	
-	// Register class auto loader
-	// Custom modules1
-	if(strpos($class, 'Cleantalk') !== false){
-		$class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-		$class_file = __DIR__ . DIRECTORY_SEPARATOR . $class . '.php';
-		if(file_exists($class_file)){
-			require_once($class_file);
-		}
-	}
+
+spl_autoload_register(function ($class) {
+    // Register class auto loader
+    // Custom modules1
+    if ( strpos($class, 'Cleantalk') !== false ) {
+        $class      = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+        $class_file = __DIR__ . DIRECTORY_SEPARATOR . $class . '.php';
+        if ( file_exists($class_file) ) {
+            require_once($class_file);
+        }
+    }
 });
-
-
