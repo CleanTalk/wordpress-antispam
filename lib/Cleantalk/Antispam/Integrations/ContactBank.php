070624ed96ab13978b3711074f1245eda3450baa
Glomberg <bazz@bk.ru>
2021-09-12 19:56:39 +0300

    Code: PSR12. lib/Cleantalk/Antispam/Integrations/ContactBank.php.

--- lib/Cleantalk/Antispam/Integrations/ContactBank.php
+++ lib/Cleantalk/Antispam/Integrations/ContactBank.php
@@ -1,23 +1,27 @@
 <?php
 
-
 namespace Cleantalk\Antispam\Integrations;
-
 
 class ContactBank extends IntegrationBase
 {
-
-    public function getDataForChecking( $argument )
+    public function getDataForChecking($argument)
     {
-        if( isset( $_REQUEST['param'] ) ) {
-            parse_str( isset( $_REQUEST['data'] ) ? base64_decode( $_REQUEST['data'] ) : '', $form_data );
+        if ( isset($_REQUEST['param']) ) {
+            parse_str(isset($_REQUEST['data']) ? base64_decode($_REQUEST['data']) : '', $form_data);
+
             return ct_get_fields_any($form_data);
         }
+
         return null;
     }
 
-    public function doBlock( $message )
+    public function doBlock($message)
     {
-        die(json_encode(array('apbct' => array('blocked' => true, 'comment' => $message,)), JSON_HEX_QUOT | JSON_HEX_TAG));
+        die(
+            json_encode(
+                array('apbct' => array('blocked' => true, 'comment' => $message,)),
+                JSON_HEX_QUOT | JSON_HEX_TAG
+            )
+        );
     }
 }
