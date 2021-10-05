f046adef5bb6b6f75756e8f64c1235b2c0acb5aa
Glomberg <bazz@bk.ru>
2021-10-05 10:41:15 +0300

    Fix: WPMS. Migrate old WPMS to the new wpms mode.

--- inc/cleantalk-updater.php
+++ inc/cleantalk-updater.php
@@ -1499,4 +1499,14 @@ function apbct_update_to_5_162_1()
         }
         $apbct->saveSettings();
     }
+
+    // Migrate old WPMS to the new wpms mode
+    if ( isset($apbct->settings['multisite__allow_custom_key']) ) {
+        if ( $apbct->settings['multisite__allow_custom_key'] == 1 ) {
+            $apbct->settings['multisite__work_mode'] = 1;
+        } else {
+            $apbct->settings['multisite__work_mode'] = 2;
+        }
+        $apbct->saveSettings();
+    }
 }
