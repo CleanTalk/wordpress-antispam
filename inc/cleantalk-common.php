7d0335de37f56b588e1768f26e1e4f41fed2fd45
Glomberg <bazz@bk.ru>
2021-11-25 19:27:01 +0300

    Fix. Antispam. Exception action argument fixed.

--- inc/cleantalk-common.php
+++ inc/cleantalk-common.php
@@ -179,7 +179,7 @@ function apbct_base_call($params = array(), $reg_flag = false)
     /**
      * Add exception_action sender email is empty
      */
-    if ( empty($params['sender_email']) ) {
+    if ( empty($params['sender_email']) && ! isset($params['exception_action']) ) {
         $params['exception_action'] = 1;
     }
 
