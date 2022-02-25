21b1ddadc2a48366b999a3c04141ae9e8ed535bb
Glomberg <bazz@bk.ru>
2022-02-25 07:27:40 +0300

    Fix. Integration. GiveWP extra requests excluded.

--- lib/Cleantalk/Antispam/Integrations/GiveWP.php
+++ lib/Cleantalk/Antispam/Integrations/GiveWP.php
@@ -2,22 +2,34 @@
 
 namespace Cleantalk\Antispam\Integrations;
 
+use Cleantalk\Variables\Post;
+
 class GiveWP extends IntegrationBase
 {
-
     public function getDataForChecking($argument)
     {
-        /**
-         * Filter for POST
-         */
-        $input_array = apply_filters('apbct__filter_post', $_POST);
+        if ( Post::get('action') === 'give_process_donation' ) {
+            /**
+             * Filter for POST
+             */
+            $input_array = apply_filters('apbct__filter_post', $_POST);
 
-        return ct_get_fields_any($input_array);
+            return ct_get_fields_any($input_array);
+        }
+
+        return null;
     }
 
+    /**
+     * @param $message
+     *
+     * @return void
+     */
     public function doBlock($message)
     {
-        give_set_error('spam_donation', $message);
+        if ( function_exists('give_set_error') ) {
+            give_set_error('spam_donation', $message);
+        }
         add_action('give_ajax_donation_errors', function () use ($message) {
             return 'Error: ' . $message;
         });
