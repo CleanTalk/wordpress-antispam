5b6b094d95aa1d20f32861ba76ff0684512f1495
alexandergull <galyshev@cleantalk.org>
2022-04-21 19:12:17 +0500

    Fix. CodeStyle.

--- lib/Cleantalk/Antispam/Integrations/Wpdiscuz.php
+++ lib/Cleantalk/Antispam/Integrations/Wpdiscuz.php
@@ -11,16 +11,12 @@ class Wpdiscuz extends IntegrationBase
          */
         $input_array = apply_filters('apbct__filter_post', $_POST);
 
-        if ( isset(
-                $input_array['wc_name'],
-                $input_array['wc_email'],
-                $input_array['wc_comment'])
-        ){
+        if ( isset($input_array['wc_name'], $input_array['wc_email'], $input_array['wc_comment']) ) {
             return array(
-                'message'=>$input_array['wc_comment'],
-                'email'=>$input_array['wc_email'],
-                'nickname'=>$input_array['wc_name'],
-                'sender_url'=> isset( $input_array['wc_website'] ) ? $input_array['wc_website'] : ''
+                'message' => $input_array['wc_comment'],
+                'email' => $input_array['wc_email'],
+                'nickname' => $input_array['wc_name'],
+                'sender_url' => isset($input_array['wc_website']) ? $input_array['wc_website'] : ''
             );
         }
 
