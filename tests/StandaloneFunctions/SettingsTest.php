22a07e949cfd9fa4b15c5063080db79e7f434684
alexandergull <galyshev@cleantalk.org>
2022-04-25 15:49:37 +0500

    Fix. Tests. Refactored tests for apbct_settings__sanitize__exclusions().

--- tests/StandaloneFunctions/SettingsTest.php
+++ tests/StandaloneFunctions/SettingsTest.php
@@ -13,29 +13,29 @@ class SettingsTest extends TestCase
 
     public function setUp()
     {
-        $this->errors =  array (
-            'sfw_update' => array (
-                0 => array (
+        $this->errors = array(
+            'sfw_update' => array(
+                0 => array(
                     'error' => 'Error test: apbct_sfw_update__process_files',
                     'error_time' => 1634895504
                 ),
-                1 => array (
+                1 => array(
                     'error' => 'Error test: apbct_sfw_update__process_files',
                     'error_time' => 1634895506
                 ),
-                'sub_type' => array (
-                    0 => array (
+                'sub_type' => array(
+                    0 => array(
                         'error' => 'Error test: apbct_sfw_update__end_of_update',
                         'error_time' => 1634895516
                     ),
                 ),
             ),
-            'cron' => array (
-                0 => array (
+            'cron' => array(
+                0 => array(
                     'error' => 'Error test: apbct_sfw_update__process_files',
                     'error_time' => 1634895506
                 ),
-                1 => array (
+                1 => array(
                     'error' => 'Error test: apbct_sfw_update__process_files',
                     'error_time' => 1634895509
                 ),
@@ -55,56 +55,64 @@ class SettingsTest extends TestCase
         $this->assertIsArray($prepared_errors);
     }
 
-    public function test_apbct_settings__sanitize__exclusions__single_valid()
+    public function test_apbct_settings__sanitize__exclusions__valid()
     {
-        $test_exclusions_array = [
-            'https://cleantalk.org',
-            'https://www.cleantalk.org',
-            'www.cleantalk.org',
-            'cleantalk.org',
+        $test_exclusions_string = "https://cleantalk.org'
+            'https://www.cleantalk.org'
+            'www.cleantalk.org'
+            'cleantalk.org'
+            'https://cleantalk.org/test'
+            'https://www.cleantalk.org/test'
+            'www.cleantalk.org/test'
+            'cleantalk.org/test'
+            'https://cleantalk.org/test/php.php'
+            'https://www.cleantalk.org/test/php.php'
+            'www.cleantalk.org/test/php.php'
+            'cleantalk.org/test/php.php'
+            'https://www.cleantalk.org/thereisamorethan128charshoweverthetestshouldwentfinethereisamorethan128charshoweverthetestshouldwentfinethereisamorethan128charshoweverthetestshouldwentfine'";
 
-            'https://cleantalk.org/test',
-            'https://www.cleantalk.org/test',
-            'www.cleantalk.org/test',
-            'cleantalk.org/test',
-
-            'https://cleantalk.org/test/php.php',
-            'https://www.cleantalk.org/test/php.php',
-            'www.cleantalk.org/test/php.php',
-            'cleantalk.org/test/php.php',
-
-            'https://www.cleantalk.org/thereisamorethan128charshoweverthetestshouldwentfinethereisamorethan128charshoweverthetestshouldwentfinethereisamorethan128charshoweverthetestshouldwentfine'
-        ];
-        $result = true;
-        $fail_on = '';
-        foreach ($test_exclusions_array as $exclusion_string) {
-            if (!apbct_settings__sanitize__exclusions($exclusion_string)){
-                $result = false;
-                $fail_on = $exclusion_string;
+        $test_data[] = 'expression';
+        $test_data[] = 'expression,';
+        $test_data[] = $test_exclusions_string;
+        $message = 'Fails on:';
+        foreach ($test_data as $key) {
+            $result = is_string(apbct_settings__sanitize__exclusions($key));
+            if (!$result) {
+                $message .= $key . ' TYPE:' . gettype($key);
                 break;
             }
         }
-        $this->assertEquals(true, $result, 'Fails on: ' . $fail_on);
+        $this->assertTrue($result, $message);
     }
 
-    public function test_apbct_settings__sanitize__exclusions__single_invalid()
+    public function test_apbct_settings__sanitize__exclusions__invalid()
     {
-        $test_exclusions_array = [
-            'Thereisahustword',
-            '<script>alert(\'(injection)\');</script>',
-            '@823h8@627sh',
-            ' ',
-            'https:// cleantal k.org/tes t',
-        ];
-        $result = true;
-        $fail_on = '';
-        foreach ($test_exclusions_array as $exclusion_string) {
-            if (apbct_settings__sanitize__exclusions($exclusion_string)){
-                $result = false;
-                $fail_on = $exclusion_string;
+        $test_data[] = array();
+        $test_data[] = $this;
+        $message = 'Fails on:';
+        foreach ($test_data as $key) {
+            $result = (bool)apbct_settings__sanitize__exclusions($key);
+            if ($result) {
+                $message .= $key . ' TYPE:' . gettype($key);
                 break;
             }
         }
-        $this->assertEquals(true, $result, 'Fails on: ' . $fail_on);
+
+        $this->assertFalse($result, $message);
+    }
+
+    public function test_apbct_settings__sanitize__exclusions__cutting()
+    {
+        $test_data = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21';
+        $this->assertEquals('1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20',
+            apbct_settings__sanitize__exclusions($test_data));
+
+        $test_data = 'https://www.cleantalk.org/thereisamorethan128charshoweverthetestshouldwentfinethereisamorethan128charshoweverthetestshouldwentfinethereisamorethan128charshoweverthetestshouldwentfine,https://www.cleantalk.org/thereisamorethan128charshoweverthetestshouldwentfinethereisamorethan128charshoweverthetestshouldwentfinethereisamorethan128charshoweverthetestshouldwentfine';
+        $this->assertEquals('https://www.cleantalk.org/thereisamorethan128charshoweverthetestshouldwentfinethereisamorethan128charshoweverthetestshouldwentfi',
+            apbct_settings__sanitize__exclusions($test_data));
+
+        $test_data = 'https://www.cleantalk.org/thereisamorethan128charshoweverthetestshouldwentfinethereisamorethan128charshoweverthetestshouldwentfinethereisamorethan128charshoweverthetestshouldwentfine,https://www.clessantalk.org/thereisamorethan128charshoweverthetestshouldwentfinethereisamorethan128charshoweverthetestshouldwentfinethereisamorethan128charshoweverthetestshouldwentfine';
+        $this->assertEquals('https://www.cleantalk.org/thereisamorethan128charshoweverthetestshouldwentfinethereisamorethan128charshoweverthetestshouldwentfi,https://www.clessantalk.org/thereisamorethan128charshoweverthetestshouldwentfinethereisamorethan128charshoweverthetestshouldwent',
+            apbct_settings__sanitize__exclusions($test_data));
     }
 }
