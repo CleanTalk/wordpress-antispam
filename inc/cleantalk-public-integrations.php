63225f93c742a3d5c69e0e1e1ff2b135fa6056ab
alexandergull <galyshev@cleantalk.org>
2022-05-31 15:38:32 +0500

    Fix. cleantalk-public-integrations.php->apbct_form__gravityForms__testSpam. Gravity forms multiple email fields fix.

--- inc/cleantalk-public-integrations.php
+++ inc/cleantalk-public-integrations.php
@@ -2816,7 +2816,7 @@ function apbct_form__gravityForms__testSpam($is_spam, $form, $entry)
         }
 
         foreach ( $form_fields_intermediate as $field ) {
-            if ( $field['f_type'] === 'email' ) {
+            if ( $field['f_type'] === 'email' && $field['f_visibility'] === 'visible') {
                 $email = $field['f_data'];
             }
 
