a6e7d6bf8d2bafa74a0d5c5144f6f5790ea7f7cc
Artem Anoshin <artem-anoshin@yandex.ru>
2022-06-16 14:49:18 +0400

    Mod: Updated gulpfile, concat some scripts

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp       = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     uglify     = require('gulp-uglify'),
     rename     = require('gulp-rename'),
-    cssmin     = require('gulp-cssmin');
+    cssmin     = require('gulp-cssmin'),
+    concat     = require('gulp-concat');
 
 // CSS COMPRESS
 gulp.task('compress-css', function () {
@@ -15,14 +16,35 @@ gulp.task('compress-css', function () {
 });
 
 // JS COMPRESS
-gulp.task('compress-js', function () {
-    return gulp.src('js/src/*.js')
+gulp.task('compress-all-js', function (cb) {
+    gulp.src('js/src/*.js')
         .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('js'));
+    cb();
 });
+
+// Bundle Create
+gulp.task('bundle-js', function (cb) {
+    gulp.src(
+        [
+            'js/bundle/apbct-public--functions.js',
+            'js/bundle/apbct-public.js',
+            'js/bundle/cleantalk-modal.js',
+        ]
+    )
+        .pipe(concat('apbct-public--functions.js'))
+        .pipe(gulp.dest('js/src'))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('js'));
+    cb();
+});
+
+gulp.task('compress-js', gulp.series('bundle-js', 'compress-all-js'));
 
 gulp.task('compress-css:watch', function () {
     gulp.watch('./css/src/*.css', gulp.parallel('compress-css'));
