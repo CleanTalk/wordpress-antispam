2eb6d1f4d938eec00f3f0a5ae9707db3d2cb0593
alexandergull <galyshev@cleantalk.org>
2022-06-24 18:09:25 +0500

    Fix. Request.php. Extend CURL timeout and low-speed detection limit.

--- lib/Cleantalk/Common/HTTP/Request.php
+++ lib/Cleantalk/Common/HTTP/Request.php
@@ -417,8 +417,8 @@ class Request
         $this->options = array_replace(
             array(
                 CURLOPT_URL            => ! is_array($this->url) ? $this->url : null,
-                CURLOPT_TIMEOUT        => 10,
-                CURLOPT_LOW_SPEED_TIME => 7,
+                CURLOPT_TIMEOUT        => 50,
+                CURLOPT_LOW_SPEED_TIME => 25,
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_CONNECTTIMEOUT => 5000,
                 CURLOPT_FORBID_REUSE   => true,
