bfb2d5f5a4a6c4664c9dd8ab2a767468049286d4
safronik <r.safronik@gmail.com>
2022-07-05 11:18:53 +0500

    Mod: Change the 'wait' message.

--- inc/cleantalk-public.php
+++ inc/cleantalk-public.php
@@ -1313,7 +1313,7 @@ function apbct_enqueue_and_localize_public_scripts()
         '_rest_url'                            => esc_url(apbct_get_rest_url()),
         'data__cookies_type'                   => $apbct->data['cookies_type'],
         'data__ajax_type'                      => $apbct->data['ajax_type'],
-        'text__wait_for_decoding'              => esc_html__('Wait for decoding...', 'cleantalk-spam-protect'),
+        'text__wait_for_decoding'              => esc_html__('Antispam by Cleantalk: Decoding contact data...', 'cleantalk-spam-protect'),
         'cookiePrefix'                         => apbct__get_cookie_prefix(),
     ));
 
