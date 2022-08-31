6bbe11bd2cb177ea482b6dad61c7dc3649ec7e9b
alexandergull <alex.g@cleantalk.org>
2022-09-01 01:02:28 +0500

    Tests. NoCookie class partially covered.

--- tests/ApbctWP/VariablesTest.php
+++ tests/ApbctWP/VariablesTest.php
@@ -9,7 +9,7 @@ require_once(CLEANTALK_PLUGIN_DIR . 'inc/cleantalk-updater.php');
 
 class VariablesTest extends TestCase
 {
-    private $db;
+    private $tests = array();
 
     public function setUp()
     {
@@ -18,27 +18,62 @@ class VariablesTest extends TestCase
         $apbct->saveData();
         apbct_run_update_actions('5.182','5.182');
 
+        $this->tests['no_cookie']['set']['good'] = array(
+            'int' => 1,
+            'bool' => true,
+            'test_name_db' => 'test_value_db',
+
+        );
+
+        $this->tests['no_cookie']['set']['bad'] = array(
+            false => 'bool',
+            null => null,
+            'array' => array('1'=>1, '2'=>'2')
+        );
+
     }
 
-    public function test_NoCookie_set_to_prop()
+    public function test_NoCookie_correct_statement()
     {
-        Cookie::set('test_name_prop','test_value_prop',0,'','',null,false, 'Lax', false);
+        global $apbct;
+        $this->assertEquals('none', $apbct->data['cookies_type']);
     }
 
-    public function test_NoCookie_set_to_db()
+    public function test_NoCookie_set()
     {
-        Cookie::set('test_name_db','test_value_db',0,'','',null,false, 'Lax', true);
+        //on prop
+        foreach($this->tests['no_cookie']['set']['good'] as $key=>$value){
+            //on prop
+            $result = Cookie::set($key,$value,0,'','',null,false, 'Lax', false);
+            $this->assertNotFalse($result, $key . '=>'. var_export($value,1));
+            //on db
+            $result = Cookie::set($key,$value,0,'','',null,false, 'Lax', true);
+            $this->assertNotFalse($result, $key . '=>'. var_export($value,1));
+        }
+
+        foreach($this->tests['no_cookie']['set']['bad'] as $key=>$value){
+            //on prop
+            $result = Cookie::set($key,$value,0,'','',null,false, 'Lax', false);
+            $this->assertFalse($result, $key . '=>'. var_export($value,1));
+            //on db
+            $result = Cookie::set($key,$value,0,'','',null,false, 'Lax', true);
+            $this->assertFalse($result, $key . '=>'. var_export($value,1));
+        }
     }
 
-    public function test_NoCookie_get_from_prop()
+    public function test_NoCookie_get()
     {
+        //$this->setUp();
         $this->assertIsString(Cookie::get('test_name_prop'));
-        $this->assertEquals('test_value_prop', Cookie::get('test_name_prop'));
-    }
+        $this->assertEquals('', Cookie::get('test_name_prop'));
 
-    public function test_NoCookie_get_from_db()
-    {
         $this->assertIsString(Cookie::get('test_name_db'));
         $this->assertEquals('test_value_db', Cookie::get('test_name_db'));
+
+        $this->assertEquals('', Cookie::get(true));
+        $this->assertEquals('', Cookie::get(11));
+        //$this->assertEquals('', Cookie::get(new ArrayObject()));
+
     }
+
 }
