380681c9ce3379da4781809b8a09e450b83d4bef
Glomberg <bazz@bk.ru>
2022-11-15 13:40:18 +0300

    Fix. Common. Rotating m-servers fixed.

--- lib/Cleantalk/Common/DNS.php
+++ lib/Cleantalk/Common/DNS.php
@@ -108,7 +108,7 @@ class DNS
         }
 
         $starttime = microtime(true);
-        $file      = @fsockopen($host, 80, $errno, $errstr, static::$max_server_timeout / 1000);
+        $file      = @fsockopen($host, 443, $errno, $errstr, static::$max_server_timeout / 1000);
         $stoptime  = microtime(true);
 
         if ( ! $file) {
