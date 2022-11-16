71fa3f9160981e3d7039c122064107dfa4a54d44
alexandergull <galyshev@cleantalk.org>
2022-11-16 12:13:15 +0500

    Fix. Escaping "&&" fixed.

--- lib/Cleantalk/ApbctWP/Escape.php
+++ lib/Cleantalk/ApbctWP/Escape.php
@@ -104,7 +104,7 @@ class Escape extends \Cleantalk\Common\Escape
                 )
             )
         ) {
-            $result = str_replace(array('&gt;', '&lt;'), array('>', '<'), $result);
+            $result = str_replace(array('&gt;', '&lt;', '&amp;'), array('>', '<', '&'), $result);
         }
         return $result;
     }
