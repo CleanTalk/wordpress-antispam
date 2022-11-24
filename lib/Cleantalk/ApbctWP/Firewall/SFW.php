963c5105a2c252942911300fe769bc5f034d3216
alexandergull <galyshev@cleantalk.org>
2022-11-24 14:08:34 +0500

    Mod + tests. SFW selection uses personal lists priority.

--- lib/Cleantalk/ApbctWP/Firewall/SFW.php
+++ lib/Cleantalk/ApbctWP/Firewall/SFW.php
@@ -161,7 +161,7 @@ class SFW extends \Cleantalk\Common\Firewall\FirewallModule
 				WHERE network IN (" . implode(',', $needles) . ")
 				AND	network = " . $current_ip_v4 . " & mask 
 				AND " . rand(1, 100000) . "  
-				ORDER BY status DESC LIMIT 1"
+				ORDER BY source DESC, status LIMIT 1"
             );
 
             $test_status = 1;
