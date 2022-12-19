6aa778b43f39f69bd04995c14fe135525a5e33ce
svfcode <svfcode@mail.ru>
2022-12-19 19:40:01 +0400

    fix phpcs

--- lib/Cleantalk/Antispam/Integrations/GiveWP.php
+++ lib/Cleantalk/Antispam/Integrations/GiveWP.php
@@ -8,7 +8,7 @@ class GiveWP extends IntegrationBase
 {
     public function getDataForChecking($argument)
     {
-        if ( Post::get('action') === 'give_process_donation' 
+        if ( Post::get('action') === 'give_process_donation'
             || (in_array(Post::get('give_action'), ['donation', 'purchase']) && Post::get('give_ajax')) ) {
             /**
              * Filter for POST
