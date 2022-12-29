a4c7d4cc527fb69e06a06df193cf7be624ac5a0e
alexandergull <galyshev@cleantalk.org>
2022-12-29 17:34:53 +0500

    Fix. Mewto flight forms skip process - form exclusions moved to upper ct_external instead of ajax process.

--- js/src/apbct-public-bundle.js
+++ js/src/apbct-public-bundle.js
@@ -2264,8 +2264,13 @@ function ct_protect_external() {
 
             if(typeof(currentForm.action) == 'string') {
 
+                //skip excluded forms
+                if ( formIsExclusion(currentForm)) {
+                    return
+                }
+
                 // Ajax checking for the integrated forms
-                if(isIntegratedForm(currentForm)) {
+                if ( isIntegratedForm(currentForm) ) {
 
                     apbctProcessExternalForm(currentForm, i, document);
 
@@ -2354,11 +2359,6 @@ function apbctProcessIframes()
 }
 
 function apbctProcessExternalForm(currentForm, iterator, documentObject) {
-
-    //process forms exclusions
-    if ( formIsExclusion(currentForm)) {
-        return
-    }
 
     const cleantalk_placeholder = document.createElement("i");
     cleantalk_placeholder.className = 'cleantalk_placeholder';
