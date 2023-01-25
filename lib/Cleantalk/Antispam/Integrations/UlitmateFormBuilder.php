de353fcd5513520cc98919b291179a00ab22c289
Glomberg <bazz@bk.ru>
2023-01-25 11:19:06 +0300

    Fix. Integration. Ultimate Form Builder collecting parameters fixed.

--- lib/Cleantalk/Antispam/Integrations/UlitmateFormBuilder.php
+++ lib/Cleantalk/Antispam/Integrations/UlitmateFormBuilder.php
@@ -12,9 +12,7 @@ class UlitmateFormBuilder extends IntegrationBase
 
         $ct_post_temp = $_POST;
 
-        if ( ! $apbct->stats['no_cookie_data_taken'] ) {
-            apbct_form__get_no_cookie_data();
-        }
+        $direct_no_cookie_data = null;
 
         //message clearance
         if ( isset($ct_post_temp['form_data']) && is_array($ct_post_temp['form_data']) && !empty($ct_post_temp['form_data']) ) {
@@ -27,7 +25,13 @@ class UlitmateFormBuilder extends IntegrationBase
                 if ( isset($value['name']) && $value['name'] === 'apbct_visible_fields' ) {
                     unset($ct_post_temp['form_data'][$_key]);
                 }
+                // prepare POST data to get parameters
+                $direct_no_cookie_data[$value['name']] = $value['value'];
             }
+        }
+
+        if ( ! $apbct->stats['no_cookie_data_taken'] ) {
+            apbct_form__get_no_cookie_data($direct_no_cookie_data);
         }
 
         //unset action
