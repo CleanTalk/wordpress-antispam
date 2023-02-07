db80865d212a62fc7d10b15d9445a3a8a6251309
svfcode <svfcode@mail.ru>
2023-02-07 23:20:06 +0400

    Add. Integrations. Add HubSpot integration, extend logic for external forms handler.

--- js/src/apbct-public-bundle.js
+++ js/src/apbct-public-bundle.js
@@ -2543,7 +2543,8 @@ function formIsExclusion(currentForm)
     ]
 
     let exclusions_by_class = [
-        'search-form' //search forms
+        'search-form', //search forms
+        'hs-form' // integrated hubspot plugin through dinamicRenderedForms logic
     ]
 
     let result = false
@@ -2852,6 +2853,101 @@ function sendAjaxCheckingFormData(form, prev, formOriginal) {
     );
 }
 
+window.onload = function() {
+    setTimeout(function () {
+        catchDinamicRenderedForm()
+    }, 1500);
+}
+
+function catchDinamicRenderedForm() {
+    let forms = document.getElementsByTagName('form')
+
+    catchDinamicRenderedFormHandler(forms)
+
+    const frames = document.getElementsByTagName('iframe');
+    if ( frames.length > 0 ) {
+        for ( let j = 0; j < frames.length; j++ ) {
+            if ( frames[j].contentDocument == null ) {
+                continue;
+            }
+
+            const iframeForms = frames[j].contentDocument.forms;
+
+            if ( iframeForms.length === 0 ) {
+                return;
+            }
+
+            catchDinamicRenderedFormHandler(iframeForms, frames[j].contentDocument)
+        }
+    }
+}
+
+function catchDinamicRenderedFormHandler(forms, documentObject = document) {
+    let neededFormIds = [];
+    for (let form of forms) {
+        if (form.id.indexOf('hsForm') !== -1) {
+            neededFormIds.push(form.id)
+        }
+    }
+
+    for (let formId of neededFormIds) {
+        let form = documentObject.getElementById(formId);
+        form.onsubmit = sendAjaxCheckingDinamicFormData
+    }
+}
+
+/**
+ * Sending Ajax for checking form data on dinamic rendered form
+ */
+function sendAjaxCheckingDinamicFormData(form) {
+    form.preventDefault()
+    form = form.target
+
+    var force_action = document.createElement("input");
+    force_action.name = 'action';
+    force_action.value = 'cleantalk_force_ajax_check';
+    force_action.type = 'hidden';
+    form.appendChild(force_action);
+
+    // Get visible fields and set cookie
+    var visible_fields = {};
+    visible_fields[0] = apbct_collect_visible_fields(form);
+    apbct_visible_fields_set_cookie( visible_fields );
+
+    var data = {};
+    var elems = form.elements;
+    elems = Array.prototype.slice.call(elems);
+
+    elems.forEach( function( elem, y ) {
+        if( elem.name === '' ) {
+            data['input_' + y] = elem.value;
+        } else {
+            data[elem.name] = elem.value;
+        }
+    });
+
+    apbct_public_sendAJAX(
+        data,
+        {
+            async: false,
+            callback: function(result) {
+                if( result.apbct === undefined || ! +result.apbct.blocked ) {
+                    form.onsubmit = null
+
+                    let subm_button = jQuery(form).find('input[type=submit]');
+                    if(subm_button.length !== 0) {
+                        subm_button[0].click();
+                        return;
+                    }
+                }
+
+                if (result.apbct !== undefined && +result.apbct.blocked) {
+                    ctParseBlockMessage(result);
+                }
+            }
+        }
+    );
+}
 function ct_check_internal(currForm){
     
 //Gathering data
