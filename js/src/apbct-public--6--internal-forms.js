801069cf715f1145f27b5879f6d97b64fa57fcad
alexandergull <galyshev@cleantalk.org>
2023-03-24 17:54:13 +0500

    ESLint. apbct-public--6--internal-forms.js done.

--- js/src/apbct-public--6--internal-forms.js
+++ js/src/apbct-public--6--internal-forms.js
@@ -1,78 +1,85 @@
-function ct_check_internal(currForm){
-    
-//Gathering data
-    var ct_data = {},
-        elems = currForm.elements;
+/**
+ * Check form as internal.
+ * @param {int} currForm Current form.
+ */
+function ctCheckInternal(currForm) {
+    //  Gathering data
+    const ctData = {};
+    const elems = currForm.elements;
+    let key;
 
-    for (var key in elems) {
-        if(elems[key].type == 'submit' || elems[key].value == undefined || elems[key].value == '')
-            continue;
-        ct_data[elems[key].name] = currForm.elements[key].value;
+    for (key in elems) {
+        if (elems[key].type !== 'submit' &&
+            elems[key].value !== undefined &&
+            elems[key].value !== '') {
+            ctData[elems[key].name] = currForm.elements[key].value;
+        }
     }
-    ct_data['action'] = 'ct_check_internal';
+    ctData.action = 'ct_check_internal';
 
-    //AJAX Request
+    //  AJAX Request
     apbct_public_sendAJAX(
-        ct_data,
+        ctData,
         {
             url: ctPublicFunctions._ajax_url,
-            callback: function (data) {
-                if(data.success === true){
+            callback: function(data) {
+                if (data.success === true) {
                     currForm.submit();
-                }else{
+                } else {
                     alert(data.data);
                     return false;
                 }
-            }
-        }
+            },
+        },
     );
 }
 
-document.addEventListener('DOMContentLoaded',function(){
-    let ct_currAction = '',
-        ct_currForm = '';
+document.addEventListener('DOMContentLoaded', function() {
+    let ctCurrAction = '';
+    let ctCurrForm = '';
 
-    if( ! +ctPublic.settings__forms__check_internal ) {
+    if ( ! +ctPublic.settings__forms__check_internal ) {
         return;
     }
 
-    let ctPrevHandler;
-	for( let i=0; i<document.forms.length; i++ ){
-		if ( typeof(document.forms[i].action) == 'string' ){
-            ct_currForm = document.forms[i];
-			ct_currAction = ct_currForm.action;
+    for ( let i=0; i<document.forms.length; i++ ) {
+        if ( typeof(document.forms[i].action) == 'string' ) {
+            ctCurrForm = document.forms[i];
+            ctCurrAction = ctCurrForm.action;
             if (
-                ct_currAction.indexOf('https?://') !== null &&                        // The protocol is obligatory
-                ct_currAction.match(ctPublic.blog_home + '.*?\.php') !== null && // Main check
-                ! ct_check_internal__is_exclude_form(ct_currAction)                  // Exclude WordPress native scripts from processing
+                // The protocol is obligatory
+                ctCurrAction.indexOf('https?://') !== null &&
+                // Main check
+                ctCurrAction.match(ctPublic.blog_home + '.*?\.php') !== null &&
+                // Exclude WordPress native scripts from processing
+                ! ctCheckInternalIsExcludedForm(ctCurrAction)
             ) {
-                ctPrevHandler = ct_currForm.click;
                 if ( typeof jQuery !== 'undefined' ) {
-                    jQuery(ct_currForm).off('**');
-                    jQuery(ct_currForm).off();
-                    jQuery(ct_currForm).on('submit', function(event){
-                        ct_check_internal(event.target);
+                    jQuery(ctCurrForm).off('**');
+                    jQuery(ctCurrForm).off();
+                    jQuery(ctCurrForm).on('submit', function(event) {
+                        ctCheckInternal(event.target);
                         return false;
                     });
                 }
             }
-		}
-	}
+        }
+    }
 });
 
 /**
  * Check by action to exclude the form checking
- * @param action string
- * @return boolean
+ * @param {string} action
+ * @return {boolean}
  */
-function ct_check_internal__is_exclude_form(action) {
+function ctCheckInternalIsExcludedForm(action) {
     // An array contains forms action need to be excluded.
-    let ct_internal_script_exclusions = [
+    const ctInternalScriptExclusions = [
         'wp-login.php', // WordPress login page
         'wp-comments-post.php', // WordPress Comments Form
     ];
 
-    return ct_internal_script_exclusions.some((item) => {
+    return ctInternalScriptExclusions.some((item) => {
         return action.match(new RegExp(ctPublic.blog_home + '.*' + item)) !== null;
     });
 }
