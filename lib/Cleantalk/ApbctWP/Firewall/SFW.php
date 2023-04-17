a9130b18ba34424de44dbbacd2cf40f6daf17a3d
svfcode <svfcode@mail.ru>
2023-04-17 18:24:46 +0400

    Fix. SFW. Made the priority list a priority, fix after test.

--- lib/Cleantalk/ApbctWP/Firewall/SFW.php
+++ lib/Cleantalk/ApbctWP/Firewall/SFW.php
@@ -180,10 +180,6 @@ class SFW extends \Cleantalk\Common\Firewall\FirewallModule
                             break;
                     }
 
-                    if ($text_status === 'PASS_SFW__BY_WHITELIST') {
-                        break;
-                    }
-
                     $result_entry = array(
                         'ip'          => $current_ip,
                         'network'     => Helper::ipLong2ip($db_result['network'])
@@ -194,6 +190,10 @@ class SFW extends \Cleantalk\Common\Firewall\FirewallModule
                     );
 
                     $test_status = (int)$db_result['status'];
+
+                    if ($text_status === 'PASS_SFW__BY_WHITELIST') {
+                        break;
+                    }
                 }
             } else {
                 $result_entry = array(
