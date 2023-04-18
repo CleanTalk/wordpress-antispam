9ab2e93caf6d6cf1833f4058b8cf93a97c40a983
Artem Anoshin <artem-anoshin@yandex.ru>
2023-04-18 07:41:00 +0400

    js/src/apbct-public--3--cleantalk-modal.js is done

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     },
     extends: 'google',
     ignorePatterns: [
-        'js/src/apbct-public--3--cleantalk-modal.js',
         'js/src/apbct-public--5--external-forms.js',
         'js/src/apbct-public-bundle.js',
         'js/src/cleantalk-admin-settings-page.js',
