ced00eb389510b6bfd9a26f81843c79f93cbca53
Artem Anoshin <artem-anoshin@yandex.ru>
2023-04-18 07:43:38 +0400

    .bundle minified

--- js/src/apbct-public-bundle.js
+++ js/src/apbct-public-bundle.js
@@ -2558,7 +2558,7 @@ function getNoCookieData() {
 }
 
 /* Cleantalk Modal object */
-let cleantalkModal = {
+const cleantalkModal = {
 
     // Flags
     loaded: false,
@@ -2568,113 +2568,114 @@ let cleantalkModal = {
 
     // Methods
     load: function( action ) {
-        if( ! this.loaded ) {
+        if ( ! this.loaded ) {
             this.loading = true;
-            let callback = function( result, data, params, obj ) {
+            const callback = function( result, data, params, obj ) {
                 cleantalkModal.loading = false;
                 cleantalkModal.loaded = result;
                 document.dispatchEvent(
-                    new CustomEvent( "cleantalkModalContentLoaded", {
+                    new CustomEvent( 'cleantalkModalContentLoaded', {
                         bubbles: true,
-                    } )
+                    } ),
                 );
             };
-            if( typeof apbct_admin_sendAJAX === "function" ) {
-                apbct_admin_sendAJAX( { 'action' : action }, { 'callback': callback, 'notJson': true } );
+            // eslint-disable-next-line camelcase
+            if ( typeof apbct_admin_sendAJAX === 'function' ) {
+                apbct_admin_sendAJAX( {'action': action}, {'callback': callback, 'notJson': true} );
             } else {
-                apbct_public_sendAJAX( { 'action' : action }, { 'callback': callback, 'notJson': true } );
+                apbct_public_sendAJAX( {'action': action}, {'callback': callback, 'notJson': true} );
             }
-
         }
     },
 
-    open: function () {
+    open: function() {
         /* Cleantalk Modal CSS start */
-        var renderCss = function () {
-            var cssStr = '';
-            for ( let key in this.styles ) {
+        const renderCss = function() {
+            let cssStr = '';
+            // eslint-disable-next-line guard-for-in
+            for ( const key in this.styles ) {
                 cssStr += key + ':' + this.styles[key] + ';';
             }
             return cssStr;
         };
-        var overlayCss = {
+        const overlayCss = {
             styles: {
-                "z-index": "9999999999",
-                "position": "fixed",
-                "top": "0",
-                "left": "0",
-                "width": "100%",
-                "height": "100%",
-                "background": "rgba(0,0,0,0.5)",
-                "display": "flex",
-                "justify-content" : "center",
-                "align-items" : "center",
+                'z-index': '9999999999',
+                'position': 'fixed',
+                'top': '0',
+                'left': '0',
+                'width': '100%',
+                'height': '100%',
+                'background': 'rgba(0,0,0,0.5)',
+                'display': 'flex',
+                'justify-content': 'center',
+                'align-items': 'center',
             },
-            toString: renderCss
+            toString: renderCss,
         };
-        var innerCss = {
+        const innerCss = {
             styles: {
-                "position" : "relative",
-                "padding" : "30px",
-                "background" : "#FFF",
-                "border" : "1px solid rgba(0,0,0,0.75)",
-                "border-radius" : "4px",
-                "box-shadow" : "7px 7px 5px 0px rgba(50,50,50,0.75)",
+                'position': 'relative',
+                'padding': '30px',
+                'background': '#FFF',
+                'border': '1px solid rgba(0,0,0,0.75)',
+                'border-radius': '4px',
+                'box-shadow': '7px 7px 5px 0px rgba(50,50,50,0.75)',
             },
-            toString: renderCss
+            toString: renderCss,
         };
-        var closeCss = {
+        const closeCss = {
             styles: {
-                "position" : "absolute",
-                "background" : "#FFF",
-                "width" : "20px",
-                "height" : "20px",
-                "border" : "2px solid rgba(0,0,0,0.75)",
-                "border-radius" : "15px",
-                "cursor" : "pointer",
-                "top" : "-8px",
-                "right" : "-8px",
-                "box-sizing" : "content-box",
+                'position': 'absolute',
+                'background': '#FFF',
+                'width': '20px',
+                'height': '20px',
+                'border': '2px solid rgba(0,0,0,0.75)',
+                'border-radius': '15px',
+                'cursor': 'pointer',
+                'top': '-8px',
+                'right': '-8px',
+                'box-sizing': 'content-box',
             },
-            toString: renderCss
+            toString: renderCss,
         };
-        var closeCssBefore = {
+        const closeCssBefore = {
             styles: {
-                "content" : "\"\"",
-                "display" : "block",
-                "position" : "absolute",
-                "background" : "#000",
-                "border-radius" : "1px",
-                "width" : "2px",
-                "height" : "16px",
-                "top" : "2px",
-                "left" : "9px",
-                "transform" : "rotate(45deg)",
+                'content': '""',
+                'display': 'block',
+                'position': 'absolute',
+                'background': '#000',
+                'border-radius': '1px',
+                'width': '2px',
+                'height': '16px',
+                'top': '2px',
+                'left': '9px',
+                'transform': 'rotate(45deg)',
             },
-            toString: renderCss
+            toString: renderCss,
         };
-        var closeCssAfter = {
+        const closeCssAfter = {
             styles: {
-                "content" : "\"\"",
-                "display" : "block",
-                "position" : "absolute",
-                "background" : "#000",
-                "border-radius" : "1px",
-                "width" : "2px",
-                "height" : "16px",
-                "top" : "2px",
-                "left" : "9px",
-                "transform" : "rotate(-45deg)",
+                'content': '""',
+                'display': 'block',
+                'position': 'absolute',
+                'background': '#000',
+                'border-radius': '1px',
+                'width': '2px',
+                'height': '16px',
+                'top': '2px',
+                'left': '9px',
+                'transform': 'rotate(-45deg)',
             },
-            toString: renderCss
+            toString: renderCss,
         };
-        var bodyCss = {
+        const bodyCss = {
             styles: {
-                "overflow" : "hidden",
+                'overflow': 'hidden',
             },
-            toString: renderCss
+            toString: renderCss,
         };
-        var cleantalkModalStyle = document.createElement( 'style' );
+        const cleantalkModalStyle = document.createElement( 'style' );
         cleantalkModalStyle.setAttribute( 'id', 'cleantalk-modal-styles' );
         cleantalkModalStyle.innerHTML = 'body.cleantalk-modal-opened{' + bodyCss + '}';
         cleantalkModalStyle.innerHTML += '#cleantalk-modal-overlay{' + overlayCss + '}';
@@ -2684,32 +2685,30 @@ let cleantalkModal = {
         document.body.append( cleantalkModalStyle );
         /* Cleantalk Modal CSS end */
 
-        var overlay = document.createElement( 'div' );
+        const overlay = document.createElement( 'div' );
         overlay.setAttribute( 'id', 'cleantalk-modal-overlay' );
         document.body.append( overlay );
 
         document.body.classList.add( 'cleantalk-modal-opened' );
 
-        var inner = document.createElement( 'div' );
+        const inner = document.createElement( 'div' );
         inner.setAttribute( 'id', 'cleantalk-modal-inner' );
         inner.setAttribute( 'style', innerCss );
         overlay.append( inner );
 
-        var close = document.createElement( 'div' );
+        const close = document.createElement( 'div' );
         close.setAttribute( 'id', 'cleantalk-modal-close' );
         inner.append( close );
 
-        var content = document.createElement( 'div' );
+        const content = document.createElement( 'div' );
         if ( this.loaded ) {
-            var urlRegex = /(https?:\/\/[^\s]+)/g;
-            var service_content_regex = /.*\/inc/g;
-            if (service_content_regex.test(this.loaded)){
-                content.innerHTML = this.loaded
+            const urlRegex = /(https?:\/\/[^\s]+)/g;
+            const serviceContentRegex = /.*\/inc/g;
+            if (serviceContentRegex.test(this.loaded)) {
+                content.innerHTML = this.loaded;
             } else {
-                var renderedMsg = this.loaded.replace(urlRegex, '<a href="$1" target="_blank">$1</a>');
-                content.innerHTML = renderedMsg;
+                content.innerHTML = this.loaded.replace(urlRegex, '<a href="$1" target="_blank">$1</a>');
             }
-
         } else {
             content.innerHTML = 'Loading...';
             // @ToDo Here is hardcoded parameter. Have to get this from a 'data-' attribute.
@@ -2721,30 +2720,31 @@ let cleantalkModal = {
         this.opened = true;
     },
 
-    close: function () {
+    close: function() {
         document.body.classList.remove( 'cleantalk-modal-opened' );
         document.getElementById( 'cleantalk-modal-overlay' ).remove();
         document.getElementById( 'cleantalk-modal-styles' ).remove();
         document.dispatchEvent(
-            new CustomEvent( "cleantalkModalClosed", {
+            new CustomEvent( 'cleantalkModalClosed', {
                 bubbles: true,
-            } )
+            } ),
         );
-    }
+    },
 
 };
 
 /* Cleantalk Modal helpers */
-document.addEventListener('click',function( e ){
-    if( e.target && (e.target.id === 'cleantalk-modal-overlay' || e.target.id === 'cleantalk-modal-close') ){
+document.addEventListener('click', function( e ) {
+    if ( e.target && (e.target.id === 'cleantalk-modal-overlay' || e.target.id === 'cleantalk-modal-close') ) {
         cleantalkModal.close();
     }
 });
-document.addEventListener("cleantalkModalContentLoaded", function( e ) {
-    if( cleantalkModal.opened && cleantalkModal.loaded ) {
+document.addEventListener('cleantalkModalContentLoaded', function( e ) {
+    if ( cleantalkModal.opened && cleantalkModal.loaded ) {
         document.getElementById( 'cleantalk-modal-content' ).innerHTML = cleantalkModal.loaded;
     }
 });
+
 const buttonsToHandle = [];
 
 document.addEventListener('DOMContentLoaded', function() {
