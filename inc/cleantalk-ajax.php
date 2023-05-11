75dc0cf66504be0c9fa8a54b045d228f1040e11f
Artem Anoshin <artem-anoshin@yandex.ru>
2023-05-11 16:18:31 +0400

    fix: fixed block message - eform

--- inc/cleantalk-ajax.php
+++ inc/cleantalk-ajax.php
@@ -1033,6 +1033,24 @@ function ct_ajax_hook($message_obj = null)
             die();
         }
 
+        // Plugin Name: eForm - WordPress Form Builder; ajax action ipt_fsqm_save_form
+        if (
+            apbct_is_plugin_active('wp-fsqm-pro/ipt_fsqm.php') &&
+            Post::get('action') === 'ipt_fsqm_save_form'
+        ) {
+            $return = array(
+                'success' => false,
+                'errors' => array(
+                    0 => array(
+                        'id' => '',
+                        'msgs' => array( $ct_result->comment ),
+                    ),
+                ),
+            );
+            echo json_encode( (object) $return );
+            die();
+        }
+
         // Regular block output
         die(
             json_encode(
