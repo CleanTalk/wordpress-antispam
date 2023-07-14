5d9d7c95d1531b02ff5d335cbf00a9094fdd47b6
Glomberg <bazz@bk.ru>
2023-07-14 10:58:52 +0300

    Fix. Alt cookies. Parameters input_focused and key_up storing fixed.

--- js/src/apbct-public--2--public.js
+++ js/src/apbct-public--2--public.js
@@ -308,7 +308,7 @@ function ctSetHasInputFocused() {
     if ( ! apbctLocalStorage.isSet('ct_has_input_focused') || ! apbctLocalStorage.get('ct_has_input_focused') ) {
         apbctLocalStorage.set('ct_has_input_focused', true);
     }
-    if (ctPublic.data__cookies_type === 'native' && ctGetCookie('ct_has_input_focused') === undefined) {
+    if ((ctPublic.data__cookies_type === 'native' || ctPublic.data__cookies_type === 'alternative') && ctGetCookie('ct_has_input_focused') === undefined) {
         ctSetCookie('ct_has_input_focused', 'true');
     }
 }
@@ -320,7 +320,7 @@ function ctSetHasKeyUp() {
     if ( ! apbctLocalStorage.isSet('ct_has_key_up') || ! apbctLocalStorage.get('ct_has_key_up') ) {
         apbctLocalStorage.set('ct_has_key_up', true);
     }
-    if (ctPublic.data__cookies_type === 'native' && ctGetCookie('ct_has_key_up') === undefined) {
+    if ((ctPublic.data__cookies_type === 'native' || ctPublic.data__cookies_type === 'alternative') && ctGetCookie('ct_has_key_up') === undefined) {
         ctSetCookie('ct_has_key_up', 'true');
     }
 }
