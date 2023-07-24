8a43c7d48e507bb76ed179129a4b309cbef7224b
Artem Anoshin <artem-anoshin@yandex.ru>
2023-07-24 08:42:36 +0400

    Fix: ESLint fix

--- js/src/apbct-public-bundle.js
+++ js/src/apbct-public-bundle.js
@@ -1732,7 +1732,7 @@ if (ctPublic.data__key_is_ok) {
     apbct_attach_event_handler(document, 'scroll', ctSetHasScrolled);
 }
 
-function apbctPrepareBlockForAjaxForms() {
+function apbctPrepareBlockForAjaxForms () {
     if (typeof jQuery !== 'undefined') {
         // Capturing responses and output block message for unknown AJAX forms
         jQuery(document).ajaxComplete(function(event, xhr, settings) {
