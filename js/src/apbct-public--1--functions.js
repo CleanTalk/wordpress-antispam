6eb17dc41c47043bfe2f78862634ee8d901315ca
SVFCode <svfcode@mail.ru>
2023-09-07 10:11:37 +0300

    New. JS. Add frontend params for typo. (#306)
    * New. JS. Add frontend params for typo.
    
    * fix eslint
    
    ---------
    
    Co-authored-by: Glomberg <bazz@bk.ru>

--- js/src/apbct-public--1--functions.js
+++ js/src/apbct-public--1--functions.js
@@ -322,3 +322,71 @@ let apbctSessionStorage = {
         return data;
     },
 };
+
+/**
+ * Handler for -webkit based browser that listen for a custom
+ * animation create using the :pseudo-selector in the stylesheet.
+ * Works with Chrome, Safari
+ *
+ * @param {AnimationEvent} event
+ */
+// eslint-disable-next-line no-unused-vars,require-jsdoc
+function apbctOnAnimationStart(event) {
+    ('onautofillstart' === event.animationName) ?
+        apbctAutocomplete(event.target) : apbctCancelAutocomplete(event.target);
+}
+
+/**
+ * Handler for non-webkit based browser that listen for input
+ * event to trigger the autocomplete-cancel process.
+ * Works with Firefox, Edge, IE11
+ *
+ * @param {InputEvent} event
+ */
+// eslint-disable-next-line no-unused-vars,require-jsdoc
+function apbctOnInput(event) {
+    ('insertReplacementText' === event.inputType || !('data' in event)) ?
+        apbctAutocomplete(event.target) : apbctCancelAutocomplete(event.target);
+}
+
+/**
+ * Manage an input element when its value is autocompleted
+ * by the browser in the following steps:
+ * - add [autocompleted] attribute from event.target
+ * - create 'onautocomplete' cancelable CustomEvent
+ * - dispatch the Event
+ *
+ * @param {HtmlInputElement} element
+ */
+function apbctAutocomplete(element) {
+    if (element.hasAttribute('autocompleted')) return;
+    element.setAttribute('autocompleted', '');
+
+    let event = new window.CustomEvent('onautocomplete', {
+        bubbles: true, cancelable: true, detail: null,
+    });
+
+    // no autofill if preventDefault is called
+    if (!element.dispatchEvent(event)) {
+        element.value = '';
+    }
+}
+
+/**
+ * Manage an input element when its autocompleted value is
+ * removed by the browser in the following steps:
+ * - remove [autocompleted] attribute from event.target
+ * - create 'onautocomplete' non-cancelable CustomEvent
+ * - dispatch the Event
+ *
+ * @param {HtmlInputElement} element
+ */
+function apbctCancelAutocomplete(element) {
+    if (!element.hasAttribute('autocompleted')) return;
+    element.removeAttribute('autocompleted');
+
+    // dispatch event
+    element.dispatchEvent(new window.CustomEvent('onautocomplete', {
+        bubbles: true, cancelable: false, detail: null,
+    }));
+}
--- js/src/apbct-public--2--public.js
+++ js/src/apbct-public--2--public.js
@@ -493,6 +493,12 @@ function apbct_ready() {
     // 2nd try to add listeners for delayed appears forms
     setTimeout(ctStartFieldsListening, 1000);
 
+    window.addEventListener('animationstart', apbctOnAnimationStart, true);
+    window.addEventListener('input', apbctOnInput, true);
+    document.ctTypoData = new CTTypoData();
+    document.ctTypoData.gatheringFields();
+    document.ctTypoData.setListeners();
+
     // Collect scrolling info
     const initCookies = [
         ['ct_ps_timestamp', Math.floor(new Date().getTime() / 1000)],
@@ -1237,7 +1243,13 @@ function ctNoCookieConstructHiddenField(type) {
     let field = '';
     let noCookieDataLocal = apbctLocalStorage.getCleanTalkData();
     let noCookieDataSession = apbctSessionStorage.getCleanTalkData();
-    let noCookieData = {...noCookieDataLocal, ...noCookieDataSession};
+
+    let noCookieDataTypo = {typo: []};
+    if (document.ctTypoData && document.ctTypoData.data) {
+        noCookieDataTypo = {typo: document.ctTypoData.data};
+    }
+
+    let noCookieData = {...noCookieDataLocal, ...noCookieDataSession, ...noCookieDataTypo};
     noCookieData = JSON.stringify(noCookieData);
     noCookieData = '_ct_no_cookie_data_' + btoa(noCookieData);
     field = document.createElement('input');
