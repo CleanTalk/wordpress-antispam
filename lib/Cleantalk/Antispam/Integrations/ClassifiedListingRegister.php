65baf8021327587de145a87a783c2e36cb63937b
Glomberg <bazz@bk.ru>
2024-02-09 13:40:18 +0300

    New. Integration. Classifield theme users registering protection implemented.

---
+++ lib/Cleantalk/Antispam/Integrations/ClassifiedListingRegister.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Cleantalk\Antispam\Integrations;
+
+class ClassifiedListingRegister extends IntegrationBase
+{
+    public function getDataForChecking($argument)
+    {
+        $nonce_value = isset($_POST['rtcl-register-nonce']) ? $_POST['rtcl-register-nonce'] : null;
+        if (
+            ! apbct_is_plugin_active('classified-listing/classified-listing.php') ||
+            empty($_POST['rtcl-register']) ||
+            ! wp_verify_nonce($nonce_value, 'rtcl-register')
+        ) {
+            return null;
+        }
+
+        $form_data['username'] = isset($_POST['username']) ? trim($_POST['username']) : '';
+        $form_data['email']    = isset($_POST['email']) ? $_POST['email'] : '';
+        if ( ! empty($_POST['first_name']) ) {
+            $form_data['first_name'] = $_POST['first_name'];
+        }
+        if ( ! empty($_POST['last_name']) ) {
+            $form_data['last_name'] = $_POST['last_name'];
+        }
+
+        /**
+         * Filter for POST
+         */
+        $input_array = apply_filters('apbct__filter_post', $form_data);
+
+        $output = ct_gfa($input_array);
+        $output['register'] = true;
+
+        return $output;
+    }
+
+    public function doBlock($message)
+    {
+        global $ct_comment;
+        $ct_comment = $message;
+        ct_die(null, null);
+    }
+}
