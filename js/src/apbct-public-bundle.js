f752e87ef240bc45e5941ca409cf2a815e67baca
alexandergull <galyshev@cleantalk.org>
2024-02-11 21:58:39 +0500

    Fix. apbctPrepareBlockForAjaxForms. The methdo $.ajaxComplete() is deprecated, use $.on("ajaxComplete", function()) instead.

--- js/src/apbct-public-bundle.js
+++ js/src/apbct-public-bundle.js
@@ -1963,20 +1963,31 @@ if (ctPublic.data__key_is_ok) {
  * Prepare block to intercept AJAX response
  */
 function apbctPrepareBlockForAjaxForms() {
+    // eslint-disable-next-line require-jsdoc
+    function ctPrepareBlockMessage(xhr) {
+        if (xhr.responseText &&
+            xhr.responseText.indexOf('"apbct') !== -1 &&
+            xhr.responseText.indexOf('DOCTYPE') === -1
+        ) {
+            try {
+                ctParseBlockMessage(JSON.parse(xhr.responseText));
+            } catch (e) {
+                console.log(e.toString());
+            }
+        }
+    }
+
     if (typeof jQuery !== 'undefined') {
         // Capturing responses and output block message for unknown AJAX forms
-        jQuery(document).ajaxComplete(function(event, xhr, settings) {
-            if (xhr.responseText &&
-                xhr.responseText.indexOf('"apbct') !== -1 &&
-                xhr.responseText.indexOf('DOCTYPE') === -1
-            ) {
-                try {
-                    ctParseBlockMessage(JSON.parse(xhr.responseText));
-                } catch (e) {
-                    console.log(e.toString());
-                }
-            }
-        });
+        if (typeof jQuery(document).ajaxComplete() !== 'function') {
+            jQuery(document).on('ajaxComplete', function(event, xhr, settings) {
+                ctPrepareBlockMessage(xhr);
+            });
+        } else {
+            jQuery(document).ajaxComplete( function(event, xhr, settings) {
+                ctPrepareBlockMessage(xhr);
+            });
+        }
     }
 }
 
