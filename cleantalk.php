b7f6b57f02da34d35da40758acabdfbd420f0a69
svfcode <svfcode@mail.ru>
2024-04-15 09:45:51 +0300

    Fix. Integrations. Added handler for KadenceBlocks.

--- cleantalk.php
+++ cleantalk.php
@@ -579,6 +579,11 @@ $apbct_active_integrations = array(
         'setting' => 'forms__contact_forms_test',
         'ajax'    => false
     ),
+    'KadenceBlocks' => array(
+        'hook'    => 'kb_process_ajax_submit',
+        'setting' => 'forms__contact_forms_test',
+        'ajax'    => true
+    ),
 );
 new  \Cleantalk\Antispam\Integrations($apbct_active_integrations, (array)$apbct->settings);
 
