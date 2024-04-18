8e33fc3380020c6af9ab13b86a261a7c4ac4fa3a
svfcode <svfcode@mail.ru>
2024-04-18 09:47:27 +0300

    Fix. Settings. Added unique check for urls.

--- cleantalk.php
+++ cleantalk.php
@@ -1646,7 +1646,7 @@ function apbct_sfw_update__download_files($urls, $direct_update = false)
     }
 
     //Reset keys
-    $urls          = array_values($urls);
+    $urls          = array_values(array_unique($urls));
     $results       = Helper::httpMultiRequest($urls, $apbct->fw_stats['updating_folder']);
     $count_urls    = count($urls);
     $count_results = count($results);
