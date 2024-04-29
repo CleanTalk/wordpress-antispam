4cced7ce1939d64e214ed345bd7d477b4d29d209
svfcode <svfcode@mail.ru>
2024-04-29 20:33:25 +0300

    Fix. Pixel. Added statement to draw logic.

--- js/src/apbct-public-bundle.js
+++ js/src/apbct-public-bundle.js
@@ -1704,11 +1704,20 @@ function checkEmail(e) {
 }
 
 /**
+ * @return {bool}
+ */
+function ctIsDrawPixel() {
+    return +ctPublic.pixel__enabled ||
+        (ctPublic.data__cookies_type === 'none' && document.querySelectorAll('img#apbct_pixel').length === 0) ||
+        (ctPublic.data__cookies_type === 'alternative' && document.querySelectorAll('img#apbct_pixel').length === 0);
+}
+
+/**
  * @param {string} pixelUrl
  */
 function ctSetPixelImg(pixelUrl) {
     ctSetCookie('apbct_pixel_url', pixelUrl);
-    if ( +ctPublic.pixel__enabled ) {
+    if ( ctIsDrawPixel() ) {
         if ( ! document.getElementById('apbct_pixel') ) {
             let insertedImg = document.createElement('img');
             insertedImg.setAttribute('alt', 'CleanTalk Pixel');
@@ -1725,7 +1734,7 @@ function ctSetPixelImg(pixelUrl) {
  * @param {string} pixelUrl
  */
 function ctSetPixelImgFromLocalstorage(pixelUrl) {
-    if ( +ctPublic.pixel__enabled ) {
+    if ( ctIsDrawPixel() ) {
         if ( ! document.getElementById('apbct_pixel') ) {
             let insertedImg = document.createElement('img');
             insertedImg.setAttribute('alt', 'CleanTalk Pixel');
@@ -2096,7 +2105,7 @@ function apbct_ready() {
     }
 
     if ( +ctPublic.pixel__setting ) {
-        if ( +ctPublic.pixel__enabled ) {
+        if ( ctIsDrawPixel() ) {
             ctGetPixelUrl();
         } else {
             initCookies.push(['apbct_pixel_url', ctPublic.pixel__url]);
