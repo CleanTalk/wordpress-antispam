3044e33bf080ca570e37245934b61d384bcb9657
Glomberg <bazz@bk.ru>
2024-05-02 06:33:33 +0300

    Fix. Code. JS compressing fixed.

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('compress-css', function () {
 
 // JS COMPRESS
 async function compress_all_js() {
-    await gulp.src(['js/src/*.js', '!js/src/apbct-public--*.js', 'js/src/apbct-public--3--cleantalk-modal.js'])
+    await gulp.src(['js/src/*.js', '!js/src/apbct-public--*.js', '!js/src/apbct-public-bundle.js', 'js/src/apbct-public--3--cleantalk-modal.js'])
         .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
@@ -27,26 +27,25 @@ async function compress_all_js() {
 }
 
 // Bundle Create
-async function bundle_js() {
+async function bundle_src_js() {
     await gulp.src('js/src/apbct-public--*.js')
-
         // Unminified bundle
         .pipe(concat('apbct-public-bundle.js'))
-        .pipe(gulp.dest('js/src/'))
+        .pipe(gulp.dest('js/src/'));
+}
 
+async function bundle_js() {
+    await gulp.src('js/src/apbct-public-bundle.js')
         .pipe(babel({
             presets: ["@babel/preset-env"],
-            plugins: ["@babel/plugin-transform-classes"]
+            plugins: ["@babel/plugin-transform-class-properties"]
         }))
-
-        // Minifying
-        .pipe(concat('apbct-public-bundle.js'))
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('js'));
 }
 
-gulp.task('compress-js', gulp.series(bundle_js, compress_all_js));
+gulp.task('compress-js', gulp.series(bundle_src_js, bundle_js, compress_all_js));
 
 gulp.task('compress-css:watch', function () {
     gulp.watch('./css/src/*.css', gulp.parallel('compress-css'));
