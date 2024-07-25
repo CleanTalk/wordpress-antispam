fcdd913c5b72f58f9299a97d7043244971aae386
alexandergull <galyshev@cleantalk.org>
2024-07-25 13:26:25 +0500

    Fix. External forms. Resolved conflict with hubspot data attr.

--- js/src/apbct-public--5--external-forms.js
+++ js/src/apbct-public--5--external-forms.js
@@ -601,10 +601,11 @@ function isIntegratedForm(formObj) {
         formId.indexOf('ihf-contact-request-form') !== -1 ||
         formAction.indexOf('crm.zoho.com') !== -1 ||
         formId.indexOf('delivra-external-form') !== -1 ||
-        ( formObj.classList !== undefined &&
-            !formObj.classList.contains('woocommerce-checkout') &&
-            formObj.hasAttribute('data-hs-cf-bound')
-        ) || // Hubspot integration in Elementor form// Hubspot integration in Elementor form
+        // todo Return to Hubspot for elementor in the future, disabled of reason https://doboard.com/1/task/9227
+        // ( formObj.classList !== undefined &&
+        //     !formObj.classList.contains('woocommerce-checkout') &&
+        //     formObj.hasAttribute('data-hs-cf-bound')
+        // ) || // Hubspot integration in Elementor form// Hubspot integration in Elementor form
         formAction.indexOf('eloqua.com') !== -1 || // Eloqua integration
         formAction.indexOf('kulahub.net') !== -1 // Kulahub integration
     ) {
