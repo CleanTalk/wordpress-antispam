268af5b922e54b26323664cf73f03e1fe9957c8d
SVFCode <svfcode@mail.ru>
2024-07-29 10:15:53 +0300

    Fix. JS. Updated compatibility with old browsers. (#419)
    * Fix. JS. Updated compatibility with old browsers.
    
    * reminify js

--- js/src/apbct-public-bundle.js
+++ js/src/apbct-public-bundle.js
@@ -1033,6 +1033,18 @@ class ApbctRest extends ApbctXhr {
     }
 }
 
+// add hasOwn
+if (!Object.prototype.hasOwn) {
+    Object.defineProperty(Object.prototype, 'hasOwn', { // eslint-disable-line
+        value: function(property) {
+            return Object.prototype.hasOwnProperty.call(this, property);
+        },
+        enumerable: false,
+        configurable: true,
+        writable: true,
+    });
+}
+
 /**
  * @param {object|array|string} cookies
  * @param {object|array|string} value
