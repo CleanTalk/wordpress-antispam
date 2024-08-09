71f008b8e211cab003a81dcda5a66da83b9d02b8
unknown <antonV1211@yandex.ru>
2024-08-09 19:36:02 +0700

    Upd. Integration OtterBlocks. Checking if the class and method exist

--- lib/Cleantalk/Antispam/Integrations/OtterBlocksForm.php
+++ lib/Cleantalk/Antispam/Integrations/OtterBlocksForm.php
@@ -14,29 +14,38 @@ class OtterBlocksForm extends IntegrationBase
     public function getDataForChecking($argument)
     {
         $this->form_data_request = $argument;
-
         Cookie::$force_alt_cookies_global = true;
-        $fields = $this->form_data_request->get_fields();
+
+        /**
+         * @psalm-suppress UndefinedClass
+         */
         if (
-            isset($fields) &&
-            is_array($fields)
+            class_exists('\ThemeIsle\GutenbergBlocks\Integration\Form_Data_Request') &&
+            $argument instanceof \ThemeIsle\GutenbergBlocks\Integration\Form_Data_Request &&
+            method_exists($this->form_data_request, 'get_fields')
         ) {
-            $form_data = [];
-            foreach ( $fields as $input_info ) {
-                if ( isset($input_info['id'], $input_info['value']) ) {
-                    $form_data[] = [
-                        $input_info['id'] => $input_info['value']
-                    ];
+            $fields = $this->form_data_request->get_fields();
+            if (
+                isset($fields) &&
+                is_array($fields)
+            ) {
+                $form_data = [];
+                foreach ( $fields as $input_info ) {
+                    if ( isset($input_info['id'], $input_info['value']) ) {
+                        $form_data[] = [
+                            $input_info['id'] => $input_info['value']
+                        ];
+                    }
                 }
-            }
-            if ( count($form_data) ) {
-                $gfa_result = ct_gfa($form_data);
-                $event_token = Cookie::get('ct_bot_detector_event_token');
-                if ( $event_token ) {
-                    $gfa_result['event_token'] = $event_token;
+                if ( count($form_data) ) {
+                    $gfa_result = ct_gfa($form_data);
+                    $event_token = Cookie::get('ct_bot_detector_event_token');
+                    if ( $event_token ) {
+                        $gfa_result['event_token'] = $event_token;
+                    }
                 }
+                return $gfa_result;
             }
-            return $gfa_result;
         }
         return $argument;
     }
@@ -47,6 +56,8 @@ class OtterBlocksForm extends IntegrationBase
      */
     public function doBlock($message)
     {
-        $this->form_data_request->set_error('110', $message);
+        if ( method_exists($this->form_data_request, 'set_error') ) {
+            $this->form_data_request->set_error('110', $message);
+        }
     }
 }
