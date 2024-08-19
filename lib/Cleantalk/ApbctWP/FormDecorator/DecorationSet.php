37b0f1aa523fd7be7bb3e5900cbc9abc2e83c136
Glomberg <bazz@bk.ru>
2024-08-19 17:38:46 +0300

    Fix. Form decoration. Handling design of the comment form fixed.

--- lib/Cleantalk/ApbctWP/FormDecorator/DecorationSet.php
+++ lib/Cleantalk/ApbctWP/FormDecorator/DecorationSet.php
@@ -2,11 +2,11 @@
 
 namespace Cleantalk\ApbctWP\FormDecorator;
 
-class DecorationSet {
+class DecorationSet
+{
+    public $text;
 
-    public function __construct()
-    {
-    }
+    public $color;
 
     public function header()
     {
@@ -34,7 +34,7 @@ class DecorationSet {
         //    display: block;
         //    position: absolute;
         //    color: #FFF;
-        $template = str_replace('%HEADER_TEXT%', $this->getHeaderText(), $template);
+        $template = str_replace('%HEADER_TEXT%', $this->text, $template);
         return $template;
     }
 
@@ -42,12 +42,12 @@ class DecorationSet {
     {
         return '
         <svg width="auto" height="auto" viewBox="0 0 595 76" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 22H91V49V76H0L25 49L0 22Z" fill="#CF2B2B"/>
-        <path d="M595 22H504V49V76H595L570 49L595 22Z" fill="#CF2B2B"/>
-        <path d="M48 22H91V49V76L48 60V45V22Z" fill="#9B1F1E"/>
-        <path d="M547 22H504V49V76L547 60V49V22Z" fill="#9B1F1E"/>
+        <path d="M0 22H91V49V76H0L25 49L0 22Z" fill="'. $this->darkenHexColor($this->color, 10) .'"/>
+        <path d="M595 22H504V49V76H595L570 49L595 22Z" fill="'. $this->darkenHexColor($this->color, 10) .'"/>
+        <path d="M48 22H91V49V76L48 60V45V22Z" fill="'. $this->darkenHexColor($this->color, 32) .'"/>
+        <path d="M547 22H504V49V76L547 60V49V22Z" fill="'. $this->darkenHexColor($this->color, 32) .'"/>
         <g filter="url(#filter0_d_3860_2299)">
-        <path d="M48 6H547V33V60H48V33V6Z" fill="#E62F2E"/>
+        <path d="M48 6H547V33V60H48V33V6Z" fill="'. $this->color .'"/>
         </g>
         <defs>
         <filter id="filter0_d_3860_2299" x="38" y="0" width="519" height="74" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
@@ -65,9 +65,32 @@ class DecorationSet {
         ';
     }
 
-    private function getHeaderText()
+    /**
+     * Darkens a given hex color by a specified percentage.
+     *
+     * This function takes a hex color code and a percentage by which to darken the color.
+     * It then returns the darker hex color code.
+     *
+     * @param string $hex The original hex color code (e.g., '#336699').
+     * @param float $percent The percentage by which to darken the color (e.g., 20 for 20%).
+     * @return string The darkened hex color code.
+     */
+    private function darkenHexColor($hex, $percent)
     {
-        return __('HAPPY 4TH JULY', 'cleantalk-spam-protect');
-    }
+        // Remove the hash at the start if it's there
+        $hex = ltrim($hex, '#');
 
+        // Convert hex to RGB
+        $r = hexdec(substr($hex, 0, 2));
+        $g = hexdec(substr($hex, 2, 2));
+        $b = hexdec(substr($hex, 4, 2));
+
+        // Calculate the darker color
+        $r = max(0, min(255, $r - ($r * $percent / 100)));
+        $g = max(0, min(255, $g - ($g * $percent / 100)));
+        $b = max(0, min(255, $b - ($b * $percent / 100)));
+
+        // Convert RGB back to hex
+        return sprintf("#%02x%02x%02x", $r, $g, $b);
+    }
 }
--- lib/Cleantalk/ApbctWP/FormDecorator/FormDecorator.php
+++ lib/Cleantalk/ApbctWP/FormDecorator/FormDecorator.php
@@ -2,13 +2,12 @@
 
 namespace Cleantalk\ApbctWP\FormDecorator;
 
-class FormDecorator {
-
+class FormDecorator
+{
     /**
      * @var DecorationSet
      */
     private $decoration_set;
-    private $form_html;
 
     /**
      * @param DecorationSet $decoration_set
@@ -16,30 +15,22 @@ class FormDecorator {
      */
     public function __construct($decoration_set)
     {
+        global $apbct;
         $this->decoration_set = $decoration_set;
-        add_action('comment_form', array($this, 'handleForm'));
+        $this->decoration_set->text = $apbct->settings['comments__form_decoration_text'];
+        $this->decoration_set->color = $apbct->settings['comments__form_decoration_color'];
+        add_action('comment_form_before', array($this, 'handleForm'));
+        add_action('comment_form_defaults', array($this, 'changeFormArguments'));
     }
 
-    private function decorate($content)
+    public function handleForm()
     {
-        error_log('CTDEBUG: [' . __FUNCTION__ . '] [$content]: ' . var_export($content,true));
-        //do there all the stuff
-        $this->form_html = $content;
-        $this->setStringBeforeString('<div id="respond" class="comment-respond">',
-            $this->decoration_set->header());
-        return $this->form_html;
+        echo $this->decoration_set->header();
     }
 
-    private function setStringBeforeString($string_to_search, $string_to_add)
+    public function changeFormArguments($args)
     {
-        $this->form_html = str_replace($string_to_search, $string_to_add, $this->form_html);
-        $this->form_html .= $string_to_search;
-    }
-
-    public function handleForm($arg)
-    {
-        $content = ob_get_contents();
-        ob_clean();
-        echo $this->decorate($content);
+        $args['class_container'] .= ' apbct_holiday_decoration';
+        return $args;
     }
 }
