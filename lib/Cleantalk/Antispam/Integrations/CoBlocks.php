e8d6b2314f50b56af3d2c9be49706b0cce9db6ea
Glomberg <bazz@bk.ru>
2024-11-14 14:34:55 +0300

    Fix. Code. Psalm notices fixed.

--- lib/Cleantalk/Antispam/Integrations/CoBlocks.php
+++ lib/Cleantalk/Antispam/Integrations/CoBlocks.php
@@ -3,6 +3,7 @@
 namespace Cleantalk\Antispam\Integrations;
 
 use Cleantalk\ApbctWP\Variables\Post;
+use Cleantalk\Common\TT;
 
 class CoBlocks extends IntegrationBase
 {
@@ -12,10 +13,18 @@ class CoBlocks extends IntegrationBase
     public function getDataForChecking($argument)
     {
 
-        $email_field_id = Post::get('email-field-id') ? sanitize_text_field(Post::get('email-field-id')) : 'field-email';
-        $name_field_id  = Post::get('name-field-id') ? sanitize_text_field(Post::get('name-field-id')) : 'field-name';
-        $email_field_value = isset(Post::get($email_field_id)['value']) ? sanitize_text_field(Post::get($email_field_id)['value']) : '';
-        $name_field_value  = isset(Post::get($name_field_id)['value']) ? sanitize_text_field(Post::get($name_field_id)['value']) : '';
+        $email_field_id = Post::get('email-field-id')
+            ? sanitize_text_field(TT::toString(Post::get('email-field-id')))
+            : 'field-email';
+        $name_field_id  = Post::get('name-field-id')
+            ? sanitize_text_field(TT::toString(Post::get('name-field-id')))
+            : 'field-name';
+        $email_field_value = isset(Post::get($email_field_id)['value'])
+            ? sanitize_text_field(TT::getArrayValueAsString(Post::get($email_field_id), 'value'))
+            : '';
+        $name_field_value  = isset(Post::get($name_field_id)['value'])
+            ? sanitize_text_field(TT::getArrayValueAsString(Post::get($name_field_id), 'value'))
+            : '';
 
         $data = ct_gfa($_POST, $email_field_value, $name_field_value);
 
