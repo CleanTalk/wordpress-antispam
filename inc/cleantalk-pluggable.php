67441afeac027de2211b5705fdc5e3424b08be5f
Glomberg <bazz@bk.ru>
2024-11-14 13:59:44 +0300

    Fix. Code. Wrong comment fixed.

--- inc/cleantalk-pluggable.php
+++ inc/cleantalk-pluggable.php
@@ -1439,7 +1439,7 @@ function apbct_is_skip_request($ajax = false)
             return 'Plugin Name: WooCommerce Product Enquiry Premium - have the direct integration';
         }
 
-        // CoBlocks requests. The plugin have the direct integration
+        // WP Discuz skip service requests. The plugin have the direct integration
         if ( apbct_is_plugin_active('wpdiscuz/class.WpdiscuzCore.php') &&
             strpos(TT::toString(Post::get('action')), 'wpdCheckNotificationType') !== false ) {
             return 'no_ajax_wpdCheckNotificationType';
