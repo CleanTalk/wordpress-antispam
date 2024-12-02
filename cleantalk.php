c02b0a6f59c3c034ae167c462d95a852f1ec4511
svfcode <svfcode@mail.ru>
2024-12-02 15:37:01 +0300

    Upd. Settings. Updated logic to clear old sessions data.

--- cleantalk.php
+++ cleantalk.php
@@ -3141,11 +3141,15 @@ function ct_cron_send_js_error_report_email()
  */
 function apbct_cron_clear_old_session_data()
 {
-    global $apbct;
+    global $wpdb;
 
-    if ( $apbct->data['cookies_type'] === 'alternative' ) {
-        \Cleantalk\ApbctWP\Variables\AltSessions::cleanFromOld();
+    $query = $wpdb->prepare('SHOW TABLES LIKE %s', $wpdb->esc_like(APBCT_TBL_SESSIONS));
+    $session_table_exists = $wpdb->get_var($query);
+    if (empty($session_table_exists)) {
+        return;
     }
+
+    \Cleantalk\ApbctWP\Variables\AltSessions::cleanFromOld();
 }
 
 /**
