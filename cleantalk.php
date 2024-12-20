3cf962cf8b0efbbac7adfcf7c1a9861a80a0c5f4
AntonV1211 <39616556+AntonV1211@users.noreply.github.com>
2024-12-20 18:29:52 +0700

    Fix. SFW Error Text. Checking whether the init or plugins_loaded hook was executed (#529)
    * Fix. SFW Error Text. Checking whether the init or plugins_loaded hook was executed
    
    * Fix. SFW Error Text. Adding a space after the function
    
    * Fix. Localization. Text domain loading fixed.
    
    ---------
    
    Co-authored-by: Glomberg <bazz@bk.ru>

--- cleantalk.php
+++ cleantalk.php
@@ -660,7 +660,7 @@ if ( ! defined('WP_ALLOW_MULTISITE') || (defined('WP_ALLOW_MULTISITE') && WP_ALL
 }
 
 // After plugin loaded - to load locale as described in manual
-add_action('plugins_loaded', 'apbct_plugin_loaded');
+add_action('init', 'apbct_plugin_loaded');
 
 if ( ! empty($apbct->settings['data__use_ajax']) &&
      ! apbct_is_in_uri('.xml') &&
@@ -875,14 +875,16 @@ function apbct_sfw__check()
     // Checking if database was outdated
     $is_sfw_outdated = $apbct->stats['sfw']['last_update_time'] + $apbct->stats['sfw']['update_period'] * 3 < time();
 
-    $apbct->errorToggle(
-        $is_sfw_outdated,
-        'sfw_outdated',
-        esc_html__(
-            'SpamFireWall database is outdated. Please, try to synchronize with the cloud.',
-            'cleantalk-spam-protect'
-        )
-    );
+    add_action('init', function () use ($apbct, $is_sfw_outdated) {
+        $apbct->errorToggle(
+            $is_sfw_outdated,
+            'sfw_outdated',
+            esc_html__(
+                'SpamFireWall database is outdated. Please, try to synchronize with the cloud.',
+                'cleantalk-spam-protect'
+            )
+        );
+    });
 
     if ( $is_sfw_outdated ) {
         return;
@@ -895,6 +897,7 @@ function apbct_sfw__check()
     $sfw_tables_names = SFW::getSFWTablesNames();
 
     if (!$sfw_tables_names) {
+        add_action('init', function () use ($apbct) {
             $apbct->errorAdd(
                 'sfw',
                 esc_html__(
@@ -902,7 +905,8 @@ function apbct_sfw__check()
                     'cleantalk-spam-protect'
                 )
             );
-            return;
+        });
+        return;
     }
 
     $firewall->loadFwModule(
