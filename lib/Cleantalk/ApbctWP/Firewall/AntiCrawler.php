cd0eaebdb6d985c9ef544f627c1488fe0eb734f9
AntonV1211 <antonV1211@yandex.ru>
2025-03-11 20:33:52 +0700

    New. Anti-Crawler. Excluding feed requests

--- lib/Cleantalk/ApbctWP/Firewall/AntiCrawler.php
+++ lib/Cleantalk/ApbctWP/Firewall/AntiCrawler.php
@@ -554,6 +554,13 @@ class AntiCrawler extends \Cleantalk\Common\Firewall\FirewallModule
             }
         }
 
+        if (defined( 'APBCT_ANCTICRAWLER_EXLC_FEED' ) && Server::get('REQUEST_URI')) {
+            if (preg_match_all('/feed/', Server::get('REQUEST_URI'))) {
+                error_log('CTDEBUG: [' . __FUNCTION__ . '] [APBCT_SKIP_AC]: SKIP)');
+                return true;
+            }
+        }
+
         //skip check if SFW test is running
         if (
             Get::get('sfw_test_ip') &&
