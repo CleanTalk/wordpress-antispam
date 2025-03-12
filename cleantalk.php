501bc5212c8e89465f5f1fd77a1008fb19132ab8
svfcode <svfcode@mail.ru>
2025-03-12 16:21:57 +0300

    Upd. Settings. Using level for banners from cloud.

--- cleantalk.php
+++ cleantalk.php
@@ -2713,6 +2713,20 @@ function ct_account_status_check($api_key = null, $process_errors = true)
         $apbct->data['notice_trial']        = TT::getArrayValueAsInt($result, 'trial', 0);
         $apbct->data['notice_review']       = TT::getArrayValueAsInt($result, 'show_review', 0);
 
+        $notice_banners = API::getNoticeBanners($api_key);
+
+        if (isset($notice_banners['operation_status'], $notice_banners['banners']) && $notice_banners['operation_status'] === 'SUCCESS') {
+            if (isset($notice_banners['banners']['TRIAL'], $notice_banners['banners']['TRIAL']['level'])) {
+                $apbct->data['notice_trial_level'] = strtolower($notice_banners['banners']['TRIAL']['level']);
+            }
+            if (isset($notice_banners['banners']['RENEW'], $notice_banners['banners']['RENEW']['level'])) {
+                $apbct->data['notice_renew_level'] = strtolower($notice_banners['banners']['RENEW']['level']);
+            }
+            if (isset($notice_banners['banners']['REVIEW'], $notice_banners['banners']['REVIEW']['level'])) {
+                $apbct->data['notice_review_level'] = strtolower($notice_banners['banners']['REVIEW']['level']);
+            }
+        }
+
         // Other
         $apbct->data['service_id']          = TT::getArrayValueAsInt($result, 'service_id', 0);
         $apbct->data['user_id']             = TT::getArrayValueAsInt($result, 'user_id', 0);
