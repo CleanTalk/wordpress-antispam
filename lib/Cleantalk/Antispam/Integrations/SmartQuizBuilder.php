458162c0f8143e16ee3ab1fabe8891672afac8e7
AntonV1211 <antonV1211@yandex.ru>
2025-03-20 22:11:48 +0700

    Upd. Integration. Smart Quiz Builder token definition

--- lib/Cleantalk/Antispam/Integrations/SmartQuizBuilder.php
+++ lib/Cleantalk/Antispam/Integrations/SmartQuizBuilder.php
@@ -1,29 +1,32 @@
 <?php
 
 namespace Cleantalk\Antispam\Integrations;
+
+use Cleantalk\ApbctWP\Variables\Cookie;
 use Cleantalk\ApbctWP\Variables\Post;
 
 class SmartQuizBuilder extends IntegrationBase
 {
     public function getDataForChecking($argument)
     {
-        if ( Post::get('action') === 'SQBSubmitQuizAjax') {
-            $input_array = apply_filters('apbct__filter_post', $_POST);
-            return ct_gfa_dto($input_array)->getArray();
-        }
-        return false;
+        Cookie::$force_alt_cookies_global = true;
+        apbct_form__get_no_cookie_data($_POST);
+        $input_array = apply_filters('apbct__filter_post', $_POST);
+        $input_array['event_token'] = Cookie::getString('ct_bot_detector_event_token');
+        return ct_gfa_dto($input_array, $input_array['email'])->getArray();
     }
 
     public function doBlock($message)
     {
-        die(
-            json_encode(
-                array(
-                    'status' => 'error',
-                    'info' => 'form_failed',
-                    'message' => $message,
+        echo json_encode(
+            array(
+                'apbct' => array(
+                    'blocked'     => true,
+                    'comment'     => $message,
+                    'stop_script' => apbct__stop_script_after_ajax_checking()
                 )
             )
         );
+        die();
     }
 }
