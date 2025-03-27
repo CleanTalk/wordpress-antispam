2c79333c5a9631bdad7ba3bab27d4948999f6b69
Glomberg <bazz@bk.ru>
2025-03-27 14:33:03 +0300

    Fix. Code. Psalm notice fixed for `lib\cleantalk-php-patch.php`.

--- lib/cleantalk-php-patch.php
+++ lib/cleantalk-php-patch.php
@@ -12,19 +12,19 @@ if (! function_exists('apache_request_headers')) {
         foreach ($_SERVER as $key => $val) {
             if (preg_match('/\AHTTP_/', $key)) {
                 $server_key = preg_replace('/\AHTTP_/', '', $key);
-                $key_parts  = explode('_', $server_key);
-                if (strlen($server_key) > 2) {
-                    foreach ($key_parts as $part_index => $part) {
-                        $key_parts[$part_index] = function_exists('mb_strtolower') ? mb_strtolower(
-                            $part
-                        ) : strtolower(
-                            $part
-                        );
-                        $key_parts[$part_index][0] = strtoupper($key_parts[$part_index][0]);
+                if ( is_string($server_key) ) {
+                    if ( strlen($server_key) > 2) {
+                        $key_parts  = explode('_', $server_key);
+                        foreach ($key_parts as $part_index => $part) {
+                            $key_parts[$part_index] = function_exists('mb_strtolower')
+                                ? mb_strtolower($part)
+                                : strtolower($part);
+                            $key_parts[$part_index][0] = strtoupper($key_parts[$part_index][0]);
+                        }
+                        $server_key = implode('-', $key_parts);
                     }
-                    $server_key = implode('-', $key_parts);
+                    $headers[$server_key] = $val;
                 }
-                $headers[$server_key] = $val;
             }
         }
 
