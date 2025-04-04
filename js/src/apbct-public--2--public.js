aacfdcf8817c2d031d45690928e794c8c2901fb3
svfcode <svfcode@mail.ru>
2025-04-04 17:20:24 +0300

    Fix. Integrations. Fixed zoho form flow.

--- js/src/apbct-public--2--public.js
+++ js/src/apbct-public--2--public.js
@@ -1012,9 +1012,11 @@ function apbct_ready() {
 
                 // Call previous submit action
                 if (event.target.onsubmit_prev instanceof Function && !ctOnsubmitPrevCallExclude(event.target)) {
-                    event.preventDefault();
+                    if (!event.target.querySelector('input[name="zc_gad"][id="zc_gad"]')) {
+                        event.preventDefault();
+                    }
                     setTimeout(function() {
-                        event.target.onsubmit_prev.call(event.target, event);
+                            event.target.onsubmit_prev.call(event.target, event);
                     }, 0);
                 }
             };
