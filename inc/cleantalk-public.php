1bad8035f334ccd14c09fb269fb99f3f9980525c
svfcode <svfcode@mail.ru>
2025-04-22 14:31:49 +0300

    fix phpcs

--- inc/cleantalk-public.php
+++ inc/cleantalk-public.php
@@ -1287,15 +1287,15 @@ function apbct_enqueue_and_localize_public_scripts()
 
     // Bot detector
     if ( $apbct->settings['data__bot_detector_enabled'] && ! apbct_bot_detector_scripts_exclusion()) {
-         wp_enqueue_script(
-            'ct_bot_detector',
-            'https://moderate.cleantalk.org/ct-bot-detector-wrapper.js',
-            [],
-            APBCT_VERSION,
-            array(
-                'in_footer' => $in_footer,
-                'strategy' => 'defer'
-                )
+        wp_enqueue_script(
+        'ct_bot_detector',
+        'https://moderate.cleantalk.org/ct-bot-detector-wrapper.js',
+        [],
+        APBCT_VERSION,
+        array(
+            'in_footer' => $in_footer,
+            'strategy' => 'defer'
+            )
         );
     }
 
