40a349840721d83c5c6eb2f6702c0b2197220011
svfcode <svfcode@mail.ru>
2025-04-25 07:01:00 +0300

    fix phpcs

--- lib/Cleantalk/ApbctWP/Cron.php
+++ lib/Cleantalk/ApbctWP/Cron.php
@@ -6,7 +6,7 @@ class Cron extends \Cleantalk\Common\Cron
 {
     /**
      * Get fresh instance of Cron
-     * 
+     *
      * @return Cron
      */
     public static function getInstance()
@@ -61,19 +61,19 @@ class Cron extends \Cleantalk\Common\Cron
                 $this->cron_option_name
             )
         );
-        
+
         if (!$result) {
             return array();
         }
-        
+
         // First unserialize the outer string
         $unserialized = unserialize($result);
-        
+
         // If the unserialized data is still a string, it needs to be unserialized again
         if (is_string($unserialized)) {
             $unserialized = unserialize($unserialized);
         }
-        
+
         return is_array($unserialized) ? $unserialized : array();
     }
 }
