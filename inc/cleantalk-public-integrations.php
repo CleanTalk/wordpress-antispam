7717630360b307c88f69be3b1dc3e4f1e2cc90bd
alexandergull <alex.g@cleantalk.org>
2025-07-08 10:39:00 +0500

    Fix. Integration. Ninja Forms. Fields collection fixed.

--- inc/cleantalk-public-integrations.php
+++ inc/cleantalk-public-integrations.php
@@ -1810,20 +1810,21 @@ function apbct_form__ninjaForms__testSpam()
             $nf_form_fields = $form_data['fields'];
             $nickname = '';
             $email = '';
-            // $fields = [];
             foreach ($nf_form_fields as $field) {
                 $field_info = $nf_form_fields_info_array[$field['id']];
-                // $fields['nf-field-' . $field['id'] . '-' . $field_info['field_type']] = $field['value'];
+                // handle nickname-like fields, add matches to existing nickname string
                 if ( stripos($field_info['field_key'], 'name') !== false ) {
-                    $nickname = $field['value'];
+                    $nickname = empty($nickname) ? $field['value'] : $nickname . ' ' . $field['value'];
                 }
-                if ( stripos($field_info['field_key'], 'email') !== false ) {
-                    $email = $field['value'];
+                // handle email-like fields, if no GFA result, set it once
+                if (empty($gfa_dto->email) && stripos($field_info['field_key'], 'email') !== false ) {
+                    $email = empty($email) ? $field['value'] : $email;
                 }
             }
-
-            $gfa_dto->nickname = $nickname;
-            $gfa_dto->email = $email;
+            // if gfa is empty, fill it with data from Ninja Forms, if not empty, append data from Ninja Forms
+            $gfa_dto->nickname = empty($gfa_dto->nickname) ? $nickname : $gfa_dto->nickname . ' ' . $nickname;
+            // if email is empty, fill it with data from Ninja Forms, if not empty, keep DTO
+            $gfa_dto->email = empty($gfa_dto->email) ? $email : $gfa_dto->email;
         }
     }
 
