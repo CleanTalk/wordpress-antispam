c80f53d2b28bb235082d987b1dede92142dd7e1c
svfcode <svfcode@mail.ru>
2025-08-05 16:00:43 +0300

    gather pull - Fix. Searchform. JS error on form class gathering fixed.

--- js/prebuild/apbct-public-bundle_gathering.js
+++ js/prebuild/apbct-public-bundle_gathering.js
@@ -2832,19 +2832,20 @@ class ApbctHandler {
      */
     searchFormMiddleware() {
         const isExclusion = (form) => {
+            let className = form.getAttribute('class');
+            if (typeof className !== 'string') {
+                className = '';
+            }
+
             return (
                 // fibosearch integration
                 form.querySelector('input.dgwt-wcas-search-input') ||
                 // hero search skip
                 form.getAttribute('id') === 'hero-search-form' ||
                 // hb booking search skip
-                form.getAttribute('class') === 'hb-booking-search-form' ||
+                className === 'hb-booking-search-form' ||
                 // events calendar skip
-                (
-                    form.getAttribute('class') !== null &&
-                    form.getAttribute('class').indexOf('tribe-events') !== -1 &&
-                    form.getAttribute('class').indexOf('search') !== -1
-                )
+                (className.indexOf('tribe-events') !== -1 && className.indexOf('search') !== -1)
             );
         };
 
