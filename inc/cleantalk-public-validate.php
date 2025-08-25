7c5d528a13ee04ccb0598646f92781620f9d4876
alexandergull <alex.g@cleantalk.org>
2025-08-25 16:49:24 +0500

    New. Integrations. BuddyPress App REST API protection. https://doboard.com/1/task/23893

--- inc/cleantalk-public-validate.php
+++ inc/cleantalk-public-validate.php
@@ -399,21 +399,6 @@ function ct_contact_form_validate()
                             ),
                         )
                     );
-                } else if (
-                    // BuddyBoss App - request from mobile app usually
-                    apbct_is_plugin_active('buddyboss-app/buddyboss-app.php') &&
-                    Server::getString('REQUEST_URI') === '/buddyboss-app/v1/signup'
-                ) {
-                    $data = [
-                        'code' => 'bp_rest_register_errors',
-                        'message' => [
-                            'signup_email' => $ct_result->comment
-                        ],
-                        'data' => [
-                            'status' => 403,
-                        ],
-                    ];
-                    wp_send_json($data);
                 } else {
                     ct_die(null, null);
                 }
