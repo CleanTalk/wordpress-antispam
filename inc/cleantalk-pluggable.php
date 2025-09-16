0aff2c73ccf29e846043c6a03ff2877f69eb102a
Glomberg <bazz@bk.ru>
2025-09-16 19:05:54 +0300

    Fix. Code. Pluggable exclusions logic fixed.

--- inc/cleantalk-pluggable.php
+++ inc/cleantalk-pluggable.php
@@ -1127,9 +1127,9 @@ function apbct_is_skip_request($ajax = false, $ajax_message_obj = array())
         //Skip RegistrationMagic service request
         if (
             apbct_is_plugin_active('custom-registration-form-builder-with-submission-manager/registration_magic.php') &&
-            Post::get('action') === 'rm_user_exists' ||
+            (Post::get('action') === 'rm_user_exists' ||
             Post::get('action') === 'check_username_validity' ||
-            Post::get('action') === 'check_email_exists'
+            Post::get('action') === 'check_email_exists')
         ) {
             return 'RegistrationMagic service request';
         }
@@ -1333,8 +1333,8 @@ function apbct_is_skip_request($ajax = false, $ajax_message_obj = array())
         //WP GeoDirectory service action
         if (
             apbct_is_plugin_active('geodirectory/geodirectory.php') &&
-            Post::get('action') === 'geodir_auto_save_post' ||
-            Post::get('action') === 'geodir_save_post'
+            (Post::get('action') === 'geodir_auto_save_post' ||
+            Post::get('action') === 'geodir_save_post')
         ) {
             return 'WP GeoDirectory service action';
         }
@@ -1354,10 +1354,10 @@ function apbct_is_skip_request($ajax = false, $ajax_message_obj = array())
                 apbct_is_plugin_active('easy-digital-downloads/easy-digital-downloads.php') ||
                 apbct_is_plugin_active('easy-digital-downloads-pro/easy-digital-downloads.php')
             ) &&
-            Post::get('action') === 'edd_add_to_cart' ||
+            (Post::get('action') === 'edd_add_to_cart' ||
             Post::get('action') === 'edd_get_shipping_rate' ||
             Post::get('action') === 'edd_check_email' ||
-            Post::get('action') === 'edd_recalculate_discounts_pro'
+            Post::get('action') === 'edd_recalculate_discounts_pro')
         ) {
             return 'Easy Digital Downloads service action';
         }
@@ -1367,8 +1367,8 @@ function apbct_is_skip_request($ajax = false, $ajax_message_obj = array())
                 apbct_is_plugin_active('bookingpress-appointment-booking/bookingpress-appointment-booking.php') ||
                 apbct_is_plugin_active('bookingpress-appointment-booking-pro/bookingpress-appointment-booking-pro.php')
             ) &&
-            Post::get('action') === 'bookingpress_pre_booking_verify_details' ||
-            Post::get('action') === 'bookingpress_book_appointment_booking'
+            (Post::get('action') === 'bookingpress_pre_booking_verify_details' ||
+            Post::get('action') === 'bookingpress_book_appointment_booking')
         ) {
             return 'BookingPress service action';
         }
@@ -1548,9 +1548,9 @@ function apbct_is_skip_request($ajax = false, $ajax_message_obj = array())
         // Exclusion of hooks from the Avada theme for the forms of the fusion form builder
         if (
             (apbct_is_theme_active('Avada') || apbct_is_theme_active('Avada Child')) &&
-            Post::get('action') === 'fusion_form_submit_form_to_database_email' ||
+            (Post::get('action') === 'fusion_form_submit_form_to_database_email' ||
             Post::get('action') === 'fusion_form_submit_form_to_email' ||
-            Post::get('action') === 'fusion_form_submit_ajax'
+            Post::get('action') === 'fusion_form_submit_ajax')
         ) {
             return 'fusion_form/avada_theme skip';
         }
@@ -1641,8 +1641,8 @@ function apbct_is_skip_request($ajax = false, $ajax_message_obj = array())
         // HappyForms skip every requests. HappyForms have the direct integration
         if ( (apbct_is_plugin_active('happyforms-upgrade/happyforms-upgrade.php') ||
               apbct_is_plugin_active('happyforms/happyforms.php')) &&
-             (Post::get('happyforms_message_nonce') !== '') ||
-             (Post::get('action') === 'happyforms_message' && Post::get('happyforms_form_id') !== '')
+             ((Post::get('happyforms_message_nonce') !== '') ||
+             (Post::get('action') === 'happyforms_message' && Post::get('happyforms_form_id') !== ''))
         ) {
             return 'happyform_skipped';
         }
@@ -1710,16 +1710,16 @@ function apbct_is_skip_request($ajax = false, $ajax_message_obj = array())
         if (
             apbct_is_plugin_active('restrict-content-pro/restrict-content-pro.php') &&
             Post::equal('rcp_action', 'login') &&
-            Post::get('rcp_user_login') ||
-            Post::get('rcp_user_pass')
+            (Post::get('rcp_user_login') ||
+            Post::get('rcp_user_pass'))
         ) {
             return 'Restrict Content Pro Login Form skip';
         }
         // APBCT service actions
         if (
             apbct_is_plugin_active('cleantalk-spam-protect/cleantalk.php') &&
-            apbct_is_in_uri('cleantalk-antispam/v1/check_email_before_post') ||
-            apbct_is_in_uri('cleantalk-antispam/v1/check_email_exist_post')
+            (apbct_is_in_uri('cleantalk-antispam/v1/check_email_before_post') ||
+            apbct_is_in_uri('cleantalk-antispam/v1/check_email_exist_post'))
         ) {
             return 'APBCT service actions';
         }
