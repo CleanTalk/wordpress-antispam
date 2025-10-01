166759e26b2960371afab723705f6d459400a0b9
Glomberg <bazz@bk.ru>
2025-10-01 08:23:55 +0300

    Fix. TRP. `Twenty Twenty-Five` theme support added.

--- js/prebuild/apbct-public-bundle_int-protection.js
+++ js/prebuild/apbct-public-bundle_int-protection.js
@@ -3870,6 +3870,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Selectors. Try to handle the WIDE range of themes.
     let themesCommentsSelector = '.apbct-trp > .comment-body *[class*="comment-author"]';
+    // For Twenty Twenty-Five theme
+    let twentyTwentyFiveCommentsSelector = '.apbct-trp > .wp-block-group *[class*="comment-author"]';
     if ( document.querySelector('.apbct-trp .comment-author .comment-author-link') ) {
         // For Spacious theme
         themesCommentsSelector = '.apbct-trp *[class*="comment-author-link"]';
@@ -3878,6 +3880,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let adminCommentsListSelector = '.apbct-trp td[class*="column-author"] > strong';
     const trpComments = document.querySelectorAll(
         themesCommentsSelector + ',' +
+        twentyTwentyFiveCommentsSelector + ',' +
         woocommerceReviewsSelector + ',' +
         adminCommentsListSelector);
 
--- js/prebuild/apbct-public-bundle_int-protection_gathering.js
+++ js/prebuild/apbct-public-bundle_int-protection_gathering.js
@@ -4746,6 +4746,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Selectors. Try to handle the WIDE range of themes.
     let themesCommentsSelector = '.apbct-trp > .comment-body *[class*="comment-author"]';
+    // For Twenty Twenty-Five theme
+    let twentyTwentyFiveCommentsSelector = '.apbct-trp > .wp-block-group *[class*="comment-author"]';
     if ( document.querySelector('.apbct-trp .comment-author .comment-author-link') ) {
         // For Spacious theme
         themesCommentsSelector = '.apbct-trp *[class*="comment-author-link"]';
@@ -4754,6 +4756,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let adminCommentsListSelector = '.apbct-trp td[class*="column-author"] > strong';
     const trpComments = document.querySelectorAll(
         themesCommentsSelector + ',' +
+        twentyTwentyFiveCommentsSelector + ',' +
         woocommerceReviewsSelector + ',' +
         adminCommentsListSelector);
 
