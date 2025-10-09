f4e9a021b28928ecf6d8107d4c7d0ee048b9193f
svfcode <svfcode@mail.ru>
2025-10-09 16:53:10 +0300

    Fix. Integrations. Updated statement to skip woo-mailerlite service requests.

--- inc/cleantalk-public-validate-skip-functions.php
+++ inc/cleantalk-public-validate-skip-functions.php
@@ -277,6 +277,11 @@ function skip_for_ct_contact_form_validate()
         '97' => (apbct_is_plugin_active('op-dashboard/op-dashboard.php') && apbct_is_in_uri('/trackFacebookCAPIEvents')),
         // WS Form has a direct integration
         '98' => (apbct_is_plugin_active('ws-form/ws-form.php') && apbct_is_in_uri('/ws-form/v1/submit')),
+        // woo-mailerlite service requests
+        '99' => (apbct_is_plugin_active('woo-mailerlite/woo-mailerlite.php') &&
+            (isset($_POST['action']) && $_POST['action'] === 'save_data')),
+        '100' => (apbct_is_plugin_active('woo-mailerlite/woo-mailerlite.php') &&
+            (isset($_POST['action']) && $_POST['action'] === 'woo_mailerlite_set_cart_email')),
     );
 
     foreach ( $exclusions as $exclusion_key => $state ) {
