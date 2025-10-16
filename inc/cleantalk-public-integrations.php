7361115b9c72a96c2695a767242007525edfe9f1
svfcode <svfcode@mail.ru>
2025-10-16 11:45:17 +0300

    fix phpcs

--- inc/cleantalk-public-integrations.php
+++ inc/cleantalk-public-integrations.php
@@ -1911,11 +1911,21 @@ function apbct_form__ninjaForms__testSpam()
                 1,
                 2
             ); // Prevent mail notification
-            add_filter('ninja_forms_run_action_type_add_to_hubspot', function($result) { return false; }, 1);
-            add_filter('ninja_forms_run_action_type_nfacds', function($result) { return false; }, 1);
-            add_filter('ninja_forms_run_action_type_save', function($result) { return false; }, 1);
-            add_filter('ninja_forms_run_action_type_successmessage', function($result) { return false; }, 1);
-            add_filter('ninja_forms_run_action_type_email', function($result) { return false; }, 1);
+            add_filter('ninja_forms_run_action_type_add_to_hubspot', function($result) {
+                return false;
+            }, 1);
+            add_filter('ninja_forms_run_action_type_nfacds', function($result) {
+                return false;
+            }, 1);
+            add_filter('ninja_forms_run_action_type_save', function($result) {
+                return false;
+            }, 1);
+            add_filter('ninja_forms_run_action_type_successmessage', function($result) {
+                return false;
+            }, 1);
+            add_filter('ninja_forms_run_action_type_email', function($result) {
+                return false;
+            }, 1);
         }
     }
 }
