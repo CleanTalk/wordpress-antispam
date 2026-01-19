258380cd1d7b93f765aabbe47953b1fe9cd1796d
alexandergull <alex.g@cleantalk.org>
2026-01-19 17:38:56 +0500

    Test codecov - master. #2

--- tests/ApbctWP/Firewall/SFWUpdateHelperTest.php
+++ tests/ApbctWP/Firewall/SFWUpdateHelperTest.php
@@ -15,7 +15,7 @@ class SFWUpdateHelperTest extends TestCase
         global $apbct;
         $this->apbctBackup = $apbct;
 
-        // Setup mock for $apbct global
+        // Setup mock $apbct global
         $apbct = new \stdClass();
         $apbct->fw_stats['multi_request_batch_size'] = 10;
     }
