f65d15cda077cf86e685e119f602517ba0564802
Glomberg <bazz@bk.ru>
2026-02-18 20:36:23 +0300

    Upd. Integration. HivePress forms protection improved.

--- inc/cleantalk-integrations-by-route.php
+++ inc/cleantalk-integrations-by-route.php
@@ -10,7 +10,12 @@ $apbct_active_rest_integrations = array(
         'rest_route'    => '/wp-recipe-maker/v1/user-rating/',
         'setting' => 'forms__contact_forms_test',
         'rest'       => true,
-    )
+    ),
+    'HivePress'         => array(
+        'rest_route'    => '/hivepress/v1/listings/',
+        'setting' => 'forms__contact_forms_test',
+        'rest'       => true,
+    ),
 );
 
 add_filter('rest_pre_dispatch', function ($result, $_, $request) use ($apbct_active_rest_integrations) {
