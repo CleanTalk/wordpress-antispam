b30c804ba553bd01413e98284d4cab80e7481126
Glomberg <bazz@bk.ru>
2026-04-14 12:00:05 +0300

    Fix. Code. Unit tests for #746 added #3.

---
+++ tests/Inc/TestCleantalkIntegrationsByRoute.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Inc;
+
+use PHPUnit\Framework\TestCase;
+
+class TestCleantalkIntegrationsByRoute extends TestCase
+{
+    public function test_apbct_active_rest_integrations_has_valid_structure()
+    {
+        $apbct_active_rest_integrations = null;
+        include CLEANTALK_PLUGIN_DIR . 'inc/cleantalk-integrations-by-route.php';
+
+        $this->assertIsArray($apbct_active_rest_integrations);
+        $this->assertNotEmpty($apbct_active_rest_integrations);
+
+        $routes = [];
+
+        foreach ($apbct_active_rest_integrations as $name => $data) {
+            $this->assertArrayHasKey('rest_route', $data, "Missing rest_route in $name");
+            $this->assertArrayHasKey('setting', $data, "Missing setting in $name");
+            $this->assertArrayHasKey('rest', $data, "Missing rest in $name");
+
+            $this->assertIsString($data['rest_route'], "rest_route in $name must be string");
+            $this->assertIsString($data['setting'], "setting in $name must be string");
+            $this->assertIsBool($data['rest'], "rest in $name must be boolean");
+
+            $this->assertStringStartsWith('/', $data['rest_route'], "rest_route in $name must start with /");
+
+            $this->assertNotContains($data['rest_route'], $routes, "Duplicate rest_route: {$data['rest_route']} in $name");
+            $routes[] = $data['rest_route'];
+        }
+    }
+}
