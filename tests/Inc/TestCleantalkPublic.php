dac3642c2977dd211084a333802dc5c6476c2c02
Glomberg <bazz@bk.ru>
2026-04-29 16:07:47 +0300

    Fix. Code. Unit test `TestCleantalkPublic` added.

---
+++ tests/Inc/TestCleantalkPublic.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Inc;
+
+use Cleantalk\ApbctWP\State;
+use PHPUnit\Framework\TestCase;
+
+class TestCleantalkPublic extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        global $apbct;
+        $apbct = new State('cleantalk', array('settings', 'data', 'errors', 'remote_calls', 'stats', 'fw_stats'));
+    }
+
+    protected function tearDown(): void
+    {
+        global $apbct;
+        unset($apbct);
+        parent::tearDown();
+    }
+
+    public function testApbctInitPixelUrlLoad()
+    {
+        // Arrange
+        global $apbct;
+        $apbct->data['bot_detector_enabled'] = 0;
+        $apbct->settings['data__pixel'] = '3';
+
+        // Act
+        apbct_init();
+
+        // Assert
+        $this->assertNotNull($apbct->pixel_url);
+    }
+
+    public function testApbctInitPixelUrlNotLoad()
+    {
+        // Arrange
+        global $apbct;
+        $apbct->data['bot_detector_enabled'] = 1;
+        $apbct->settings['data__pixel'] = '3';
+
+        // Act
+        apbct_init();
+
+        // Assert
+        $this->assertNull($apbct->pixel_url);
+    }
+}
