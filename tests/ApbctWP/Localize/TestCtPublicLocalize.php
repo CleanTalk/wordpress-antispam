33ed070c7c3f26ae6d551dd3b6e9746034c2a7a3
datorik <datorik@gmail.com>
2026-05-18 07:38:38 +0300

    Unit tests

--- tests/ApbctWP/Localize/TestCtPublicLocalize.php
+++ tests/ApbctWP/Localize/TestCtPublicLocalize.php
@@ -30,4 +30,30 @@ class TestCtPublicLocalize extends \PHPUnit\Framework\TestCase
         $this->assertArrayHasKey('bot_detector_enabled', $localize_data);
         $this->assertArrayNotHasKey('data__bot_detector_enabled', $localize_data);
     }
+
+    public function testGetDataContainsLinksArray()
+    {
+        $localize_data = CtPublicLocalize::getData();
+
+        $this->assertArrayHasKey('links', $localize_data);
+        $this->assertIsArray($localize_data['links']);
+        $this->assertArrayHasKey('users_editscreen', $localize_data['links']);
+        $this->assertArrayHasKey('comments_editscreen', $localize_data['links']);
+    }
+
+    public function testGetDataLinksUsersEditscreen()
+    {
+        $localize_data = CtPublicLocalize::getData();
+
+        $expected = 'https://cleantalk.org/blacklists/{TARGET}?utm_source=admin_side&utm_medium=comments&utm_content=avatar&utm_campaign=apbct_links';
+        $this->assertSame($expected, $localize_data['links']['users_editscreen']);
+    }
+
+    public function testGetDataLinksCommentsEditscreen()
+    {
+        $localize_data = CtPublicLocalize::getData();
+
+        $expected = 'https://cleantalk.org/blacklists/{TARGET}?utm_source=admin_side&utm_medium=comments&utm_content=avatar&utm_campaign=apbct_links';
+        $this->assertSame($expected, $localize_data['links']['comments_editscreen']);
+    }
 }
