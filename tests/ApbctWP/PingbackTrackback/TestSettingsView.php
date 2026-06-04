8754803f5948a7586dc2831c55c64a233361efb2
alexandergull <alex.g@cleantalk.org>
2026-06-04 14:21:04 +0500

    Upd. PHPUnit. PingbackTrackback\SettingsView

---
+++ tests/ApbctWP/PingbackTrackback/TestSettingsView.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ApbctWP\PingbackTrackback;
+
+use Cleantalk\ApbctWP\PingbackTrackback\SettingsView;
+use PHPUnit\Framework\TestCase;
+
+class TestSettingsView extends TestCase
+{
+
+    public function testRegisterTrackbackHook()
+    {
+        $this->assertIsString(
+            SettingsView::getTitle()
+        );
+        $this->assertIsString(
+            SettingsView::getDescription()
+        );
+        $this->assertIsString(
+            SettingsView::getLongDescription()
+        );
+        $this->assertNotEmpty(
+            SettingsView::getTitle()
+        );
+        $this->assertNotEmpty(
+            SettingsView::getDescription()
+        );
+        $this->assertNotEmpty(
+            SettingsView::getLongDescription()
+        );
+        $this->assertStringContainsString('<p>',
+            SettingsView::getLongDescription()
+        );
+    }
+}
