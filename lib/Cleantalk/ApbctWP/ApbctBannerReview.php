c508c1a5c955f1668302ce62788bf3a657c095ba
AntonV1211 <antonV1211@yandex.ru>
2026-06-23 20:10:29 +0700

    Fix review copilot

--- lib/Cleantalk/ApbctWP/ApbctBannerReview.php
+++ lib/Cleantalk/ApbctWP/ApbctBannerReview.php
@@ -52,7 +52,7 @@ class ApbctBannerReview extends ApbctUniversalBanner
         <div class="apbct-notice notice apbct-banner-success is-dismissible" id="<?php echo esc_attr($banner_id); ?>">
             <div class="apbct-banner-content" style="display: block;margin-bottom: 16px;">
                 <div style="display: flex;">
-                    <img src="<?php echo esc_url($logo_url); ?>" width="111px" height="24" alt="CleanTalk Logo">
+                    <img src="<?php echo esc_url($logo_url); ?>" width="111" height="24" alt="CleanTalk Logo">
                     <span style="height: 24px;margin: 0 16px;text-align: center;color: #AAAAAA;border-left: 1px solid;"></span>
                     <a href="<?php echo esc_url($this->settings_link); ?>" class="apbct-banner-link">
                         <?php esc_html_e('Anti-Spam', 'cleantalk-spam-protect'); ?>
@@ -60,7 +60,7 @@ class ApbctBannerReview extends ApbctUniversalBanner
                 </div>
                 <div class="apbct-banner-text-wrapper" style="display: flex;flex-wrap: wrap;justify-content: space-between;">
                     <div style="display: flex; gap: 32px;">
-                        <img src="<?php echo esc_url($review_img_url); ?>" width="92px" height="81px" alt="CleanTalk Review Logo" style="align-self: center;">
+                        <img src="<?php echo esc_url($review_img_url); ?>" width="92" height="81" alt="CleanTalk Review Logo" style="align-self: center;">
                         <div>
                             <div class="apbct-banner-title">
                                 <?php echo esc_html($data->text); ?>
@@ -71,7 +71,7 @@ class ApbctBannerReview extends ApbctUniversalBanner
                         </div>
                     </div>
                     <div class="apbct-banner-button-wrapper">
-                        <a href="<?php echo esc_url($data->button_url); ?>" target="_blank" class="apbct-banner-button apbct-banner-button-green"><?php echo esc_html($data->button_text); ?></a>
+                        <a href="<?php echo esc_url($data->button_url); ?>" target="_blank" rel="noopener noreferrer" class="apbct-banner-button apbct-banner-button-green"><?php echo esc_html($data->button_text); ?></a>
                         <a href="#" class="notice-dismiss-link apbct-banner-dismiss-link" onclick="jQuery(this).closest('.apbct-notice').find('.notice-dismiss').click(); return false;"><?php echo esc_html($data->additional_text); ?></a>
                     </div>
                 </div>
