7487a7ed0a87c4479cb09df25c5b7f69063bd1d5
svfcode <svfcode@mail.ru>
2026-07-16 14:22:54 +0300

    fix phpcs

--- lib/Cleantalk/ApbctWP/ContactsEncoder/Shortcodes/ExcludedEncodeContentSC.php
+++ lib/Cleantalk/ApbctWP/ContactsEncoder/Shortcodes/ExcludedEncodeContentSC.php
@@ -60,7 +60,7 @@ class ExcludedEncodeContentSC extends EmailEncoderShortCode
             if ( $this->getCurrentAction() !== 'shutdown' ) {
                 return $content;
             }
-            if ( $content === '' || $content === null ) {
+            if ( $content === '' ) {
                 $content = $apbct->buffer;
             }
         }
