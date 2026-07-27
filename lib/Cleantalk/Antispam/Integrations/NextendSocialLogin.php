71a698696ca228a0926128a232e56c3ba6e16fdc
alexandergull <alex.g@cleantalk.org>
2026-07-28 00:00:49 +0500

    Upd. Integrations. Nextend Social Login: set altsession mode if Google auth provided and set up.

--- lib/Cleantalk/Antispam/Integrations/NextendSocialLogin.php
+++ lib/Cleantalk/Antispam/Integrations/NextendSocialLogin.php
@@ -17,6 +17,7 @@ class NextendSocialLogin extends IntegrationBase
                 )
             );
         }
+        return null;
     }
 
     public function doBlock($message)
@@ -24,5 +25,25 @@ class NextendSocialLogin extends IntegrationBase
         global $ct_comment;
         $ct_comment = $message;
         ct_die(null, null);
+    }
+
+    /**
+     * Checks if the OAuth provider is enabled by verifying the saved configuration.
+     *
+     * @param string $provider_id The ID of the OAuth provider.
+     * @return bool Returns true if the OAuth provider is enabled and properly configured; otherwise, returns false.
+     */
+    public static function isOAuthProviderEnabled($provider_id)
+    {
+        if ( ! $provider_id || ! is_string($provider_id) ) {
+            return false;
+        }
+        try {
+            $option = get_option($provider_id);
+            $object = maybe_unserialize($option);
+            return is_array($object) && !empty($object['client_secret']);
+        } catch (\Throwable $e) {
+            return false;
+        }
     }
 }
--- tests/ApbctTestCase.php
+++ tests/ApbctTestCase.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 class ApbctTestCase extends TestCase
 {
     /**
-     * @var \CleantalkSP\SpbctWP\State
+     * @var State
      */
     protected static $state_storage;
 
