fe39e047f2a518d92f66d13db288cf15cf9c2997
alexandergull <alex.g@cleantalk.org>
2026-09-11 15:18:55 +0500

    Fix. Psalm doc-block notice.

--- lib/Cleantalk/Common/Helper.php
+++ lib/Cleantalk/Common/Helper.php
@@ -593,16 +593,16 @@ class Helper
      * Resolve DNS to IP
      *
      * @param      $host
-     * @param bool $out
      *
-     * @return bool
+     * @return false|string
      * @psalm-suppress PossiblyUnusedMethod
      */
-    public static function dnsResolve($host, $out = false)
+    public static function dnsResolve($host)
     {
+        $out = false;
         // Validate/normalize host (accept hostname or IP; URLs and host:port are also supported)
         if ( ! $host || ! is_string($host) ) {
-            return $out;
+            return false;
         }
 
         if ( strpos($host, '://') !== false ) {
@@ -620,7 +620,7 @@ class Helper
 
         if ( ! filter_var($host, FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME)
              && ! $is_ip ) {
-            return $out;
+            return false;
         }
 
         if ( $is_ip ) {
