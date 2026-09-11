f37f12ebd8f3400f379836fddcfda37cd2af5c11
svedge <sergey.frolenko123@gmail.com>
2026-09-11 17:19:22 +0500

    Fix. Code. Fixed compatibility with php8.5. (#888)
    * Fix. Code. Fixed compatibility with php8.5.
    
    * Fix. Code. Fixed psalm
    
    * Fix. Code. upd CE
    
    * merge dev
    
    ---------
    
    Co-authored-by: svedge <svedge@users.noreply.github.com>

--- lib/Cleantalk/Common/Helper.php
+++ lib/Cleantalk/Common/Helper.php
@@ -929,7 +929,9 @@ class Helper
         } elseif (function_exists('finfo_open')) {
             $finfo = finfo_open(FILEINFO_MIME_TYPE);
             $type  = finfo_buffer($finfo, $data);
-            finfo_close($finfo);
+            if (PHP_VERSION_ID < 80000) {
+                finfo_close($finfo);
+            }
         }
 
         // @ToDo the method must return comparison result: return $type ===  mime_content_type($data)
